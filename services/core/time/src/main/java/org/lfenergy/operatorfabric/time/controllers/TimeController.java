7028eade71a674409ac36e62b0db0542bebbb8a8
David Binder <david.binder@rte-france.com>
2018-10-26 12:31:25 +0200

    [time] Fix major code smells (See #14)

--- services/core/time/src/main/java/org/lfenergy/operatorfabric/time/controllers/TimeController.java
+++ services/core/time/src/main/java/org/lfenergy/operatorfabric/time/controllers/TimeController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.Instant;
 
-//import org.springframework.security.oauth2.provider.OAuth2Authentication;
-
 @RestController
 @RequestMapping("time")
 @Slf4j
@@ -54,7 +52,7 @@ public class TimeController  {
     public TimeData setTime(@AuthenticationPrincipal User user, @ApiParam(value = "Time Data to update time " +
        "service"  )  @Valid
                                 @RequestBody TimeData
-                            time) throws Exception {
+                            time) {
         if(user ==null){
             log.info("Time updated by unknown user");
         }else{
@@ -74,33 +72,33 @@ public class TimeController  {
     //@PreAuthorize("#oauth2.hasScope('write')")
     public TimeData updateTime(@AuthenticationPrincipal User user, @ApiParam(value = "Time Data to update " +
        "time service"  )
-    @Valid @RequestBody TimeData time) throws Exception {
+    @Valid @RequestBody TimeData time) {
         return setTime(user, time);
     }
 
     @RequestMapping(value = "/speed",
        method = RequestMethod.POST)
-    public SpeedEnum setCurrentSpeed(@Valid @RequestBody SpeedEnum currentspeed) throws Exception {
+    public SpeedEnum setCurrentSpeed(@Valid @RequestBody SpeedEnum currentspeed) {
         this.timeService.setSpeed(currentspeed);
         return this.timeService.getSpeed();
     }
 
     @RequestMapping(value = "/current",
        method = RequestMethod.POST)
-    public Long setCurrentTime(@Valid @RequestBody java.lang.Long currentTime) throws Exception {
+    public Long setCurrentTime(@Valid @RequestBody java.lang.Long currentTime) {
         this.timeService.setTime(Instant.ofEpochMilli(currentTime));
         return this.timeService.computeNow().toEpochMilli();
     }
 
     @RequestMapping(value = "/current",
        method = RequestMethod.PUT)
-    public Long updateCurrentTime(@Valid @RequestBody java.lang.Long currentTime) throws Exception {
+    public Long updateCurrentTime(@Valid @RequestBody java.lang.Long currentTime) {
         return setCurrentTime(currentTime);
     }
 
     @RequestMapping(value = "/speed",
        method = RequestMethod.PUT)
-    public SpeedEnum updateCurrentspeed(@Valid @RequestBody SpeedEnum currentspeed) throws Exception {
+    public SpeedEnum updateCurrentspeed(@Valid @RequestBody SpeedEnum currentspeed) {
         return setCurrentSpeed(currentspeed);
     }
 }
