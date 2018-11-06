a37d99e726d1fb826eb18772b1fda3bfcdbaa89b
David Binder <david.binder@rte-france.com>
2018-11-06 17:13:40 +0100

    [Project configuration] Fix travis configuration bug

--- bin/travis/upload_doc.sh
+++ bin/travis/upload_doc.sh
@@ -7,6 +7,8 @@ GH_REPO=github.com/opfab/operatorfabric.git
 HTTP_REPO="https://davidbinderRTE:${GH_DOC_TOKEN}@${GH_REPO}"
 git clone $HTTP_REPO $HOME/documentation
 version=$(echo "$OF_VERSION"| sed s/-SNAPSHOT//)
+cd $OF_HOME
+gradlew generateSwaggerCodeDoc javadoc
 for prj in "${OF_REL_COMPONENTS[@]}"; do
   echo "copying $prj documentation"
   mkdir -p $HOME/documentation/projects/$prj/$version/
