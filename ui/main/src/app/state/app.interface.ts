a511aa885b52a29ae891e37eabc445c3f2f2ee20
David Binder <david.binder@rte-france.com>
2018-11-08 14:03:57 +0100

    [all ts files] insert new licence headers

--- ui/main/src/app/state/app.interface.ts
+++ ui/main/src/app/state/app.interface.ts
@@ -1,3 +1,10 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {RouterReducerState} from '@ngrx/router-store';
 import {RouterStateUrl} from './shared/utils';
 import {State as CardState} from './light-card/light-card.reducer';
