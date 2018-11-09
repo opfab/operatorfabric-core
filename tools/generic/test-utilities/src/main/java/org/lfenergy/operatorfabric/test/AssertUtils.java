0b9d0b54082210127ee0396b6e13e2fb7c243c18
David Binder <david.binder@rte-france.com>
2018-11-09 12:39:20 +0100

    [test-utilities] Fix missing files in repo

---
+++ tools/generic/test-utilities/src/main/java/org/lfenergy/operatorfabric/test/AssertUtils.java
@@ -0,0 +1,34 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.lfenergy.operatorfabric.test;
+
+import org.assertj.core.api.ThrowableTypeAssert;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * Collection of assertion utilities
+ *
+ * @author David Binder
+ */
+public class AssertUtils {
+
+  /**
+   * Utility class don't need to be instantiated;
+   */
+  private AssertUtils(){};
+
+  /**
+   * More compact {@linkplain org.assertj.core.api.Assertions#assertThatExceptionOfType}
+   * @param exceptionType
+   * @return
+   */
+  public static ThrowableTypeAssert<? extends Throwable> assertException(Class<? extends Throwable> exceptionType) {
+    return assertThatExceptionOfType(exceptionType);
+  }
+}
