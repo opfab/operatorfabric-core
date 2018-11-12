53a4cb7b086700978d38ba6ec3576a3a57f8e7c8
David Binder <david.binder@rte-france.com>
2018-11-12 12:24:29 +0100

    [cards-publication] Typo broked compilation: fixed

--- services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/cards/publication/config/WebFluxConfig.java
+++ services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/cards/publication/config/WebFluxConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 public class WebFluxConfig {
   /**
    * Configures CORS
+   *
    * @return element of {@link WebFluxConfigurerComposite}
    */
   @Bean
