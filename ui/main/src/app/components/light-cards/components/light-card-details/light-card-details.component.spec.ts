1ade40103384a2b9a277710193c9efebd1134cc8
LE GALL Ronan <ronan.le-gall@rte-france.com>
2018-11-20 16:56:45 +0100

    [User Interface] corrects test to pass

--- ui/main/src/app/components/light-cards/components/light-card-details/light-card-details.component.spec.ts
+++ ui/main/src/app/components/light-cards/components/light-card-details/light-card-details.component.spec.ts
@@ -11,6 +11,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {LightCardDetailsComponent} from './light-card-details.component';
 import {I18nData, LightCard, Severity} from '@state/light-card/light-card.model';
 import {MatButtonModule, MatCardModule} from '@angular/material';
+import {getOneRandomLigthCard} from '@tests/helpers';
 
 describe('LightCardDetailsComponent', () => {
     let lightCardDetailsComp: LightCardDetailsComponent;
@@ -30,33 +31,27 @@ describe('LightCardDetailsComponent', () => {
     });
 
     it('should create', () => {
-        const i18nData = new I18nData('title-key', ['title-string-0']);
-        const title = i18nData;
-        expect(title).toBeTruthy();
-        expect(title.key).toEqual('title-key');
+        const lightCard = getOneRandomLigthCard();
 
-        const summary = new I18nData('summary-key', ['summary-string-0']);
-        expect(lightCardDetailsComp).toBeTruthy();
-        const lightCard = new LightCard('uid'
-            , 'id'
-            , 1
-            , 2
-            , Severity.QUESTION
-            , 'mainRecipient'
-            , 'processId'
-            , 3
-            , title
-            , summary
-        ) as LightCard;
+        // extract expected data
+        const id = lightCard.id;
+        const uid = lightCard.uid;
+        const title = lightCard.title.key;
+        const summaryValue = lightCard.summary.key;
 
-        expect(lightCard.title).toEqual(title);
         lightCardDetailsComp.lightCard = lightCard;
 
         fixture.detectChanges();
 
         expect(fixture.nativeElement.querySelector('mat-card-title').innerText)
-            .toEqual('title-key-id-uid');
+            .toEqual(id);
+        expect(fixture.nativeElement.querySelector('mat-card-subtitle').innerText)
+            .toEqual(`${title}
+${uid}`);
+        expect(fixture.nativeElement.querySelector('mat-card-content').innerText)
+            .toEqual(`${summaryValue}
 
+`);
     });
 
 });
