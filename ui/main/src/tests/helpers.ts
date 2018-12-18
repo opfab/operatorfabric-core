be804d3a6ec6f6798b669900db459e2841e23da6
David Binder <david.binder@rte-france.com>
2018-12-18 19:31:34 +0100

    [UI] adding missing headers to ts files

--- ui/main/src/tests/helpers.ts
+++ ui/main/src/tests/helpers.ts
@@ -1,3 +1,10 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {I18nData, LightCard, Severity} from '@ofModel/light-card.model';
 import {CardOperation, CardOperationType} from '@ofModel/card-operation.model';
 //
