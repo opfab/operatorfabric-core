49bd7d285ed6de267e4ee5201504b9a1b3492e9a
David Binder <david.binder@rte-france.com>
2019-01-29 10:34:07 +0100

    [UI] Adding missing copyright headers to .ts files

--- ui/main/src/app/modules/cards/services/thirds.service.ts
+++ ui/main/src/app/modules/cards/services/thirds.service.ts
@@ -1,3 +1,10 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {Injectable, Injector, OnInit} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "@env/environment";
