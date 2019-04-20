efe977ac3fa7ad2e4e9115809f3aa97b8518afe1
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-04-20 22:37:13 +0200

    [Tests] Made MockOpFabUser use OpFabJwtAuthenticationToken to be closer to reality

--- tools/spring/spring-test-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/test/WithMockOpFabUserSecurityContextFactory.java
+++ tools/spring/spring-test-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/test/WithMockOpFabUserSecurityContextFactory.java
@@ -7,13 +7,22 @@
 
 package org.lfenergy.operatorfabric.springtools.configuration.test;
 
+import org.lfenergy.operatorfabric.springtools.configuration.oauth.OAuth2JwtProcessingUtilities;
+import org.lfenergy.operatorfabric.springtools.configuration.oauth.OpFabJwtAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Creates {@link SecurityContext} containing token holding {@link WithMockOpFabUser} principal
@@ -32,7 +41,20 @@ public class WithMockOpFabUserSecurityContextFactory implements WithSecurityCont
                         customUser.lastName(),
                 Arrays.asList(customUser.roles()));
 
-        Authentication auth = new UsernamePasswordAuthenticationToken(principal, "password", principal.getAuthorities());
+        //Authentication auth = new UsernamePasswordAuthenticationToken(principal, "password", principal.getAuthorities());
+        //TODO do the same for User service if proven useful
+        String tokenValue = "dummyTokenValue";
+        Instant issuedAt = Instant.now();
+        Instant expiresAt = Instant.now().plus(365, ChronoUnit.DAYS);
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("dummyHeaderKey","dummyHeaderValue");
+        Map<String, Object> claim = new HashMap<>();
+        claim.put("sub",customUser.login());
+        Collection<GrantedAuthority> authorities = OAuth2JwtProcessingUtilities.computeAuthorities(principal);
+
+        Authentication auth = new OpFabJwtAuthenticationToken(
+                new Jwt(tokenValue, issuedAt,expiresAt,headers,claim
+                ),principal,authorities);
 
         context.setAuthentication(auth);
 
