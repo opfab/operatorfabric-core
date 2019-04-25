opfab/operatorfabric-core

commit 27728a24a448eda331b8784da07ca8f1861af980

[Minor] Renamed config packages to configuration (except Config Service)

tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/BusConfiguration.java
@@ -1,0 +1,23 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.lfenergy.operatorfabric.springtools.configuration.oauth;
+
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>This configuration class registers all custom events from specified basePackages with Spring Cloud Bus.</p>
+ * Created on 12/02/19
+ *
+ * @author Alexandra Guironnet
+ */
+@Configuration
+@RemoteApplicationEventScan(basePackages = "org.lfenergy.operatorfabric.springtools.config.oauth")
+public class BusConfiguration {
+
+}

tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/config/oauth/BusConfiguration.java
@@ -1,23 +1,0 @@
-/* Copyright (c) 2018, RTE (http://www.rte-france.com)
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
-package org.lfenergy.operatorfabric.springtools.config.oauth;
-
-import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * <p>This configuration class registers all custom events from specified basePackages with Spring Cloud Bus.</p>
- * Created on 12/02/19
- *
- * @author Alexandra Guironnet
- */
-@Configuration
-@RemoteApplicationEventScan(basePackages = "org.lfenergy.operatorfabric.springtools.config.oauth")
-public class BusConfiguration {
-
-}

commit 65549d72b999ccc3950cdde958791bd2fecdbe87

[OC-225] #time 20m #done Fixes user cache bug introduced by 27728a2

tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/BusConfiguration.java
@@ -11,13 +11,13 @@ import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * <p>This configuration class registers all custom events from specified basePackages with Spring Cloud Bus.</p>
+ * <p>This configuration class registers all custom events from current package with Spring Cloud Bus.</p>
  * Created on 12/02/19
  *
  * @author Alexandra Guironnet
  */
 @Configuration
-@RemoteApplicationEventScan(basePackages = "org.lfenergy.operatorfabric.springtools.config.oauth")
+@RemoteApplicationEventScan
 public class BusConfiguration {
 
 }
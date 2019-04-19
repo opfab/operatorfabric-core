65549d72b999ccc3950cdde958791bd2fecdbe87
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-04-19 17:41:20 +0200

    [OC-225] #time 20m #done Fixes user cache bug introduced by 27728a2

--- tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/BusConfiguration.java
+++ tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/BusConfiguration.java
@@ -11,13 +11,13 @@ import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * <p>This configuration class registers all custom events from specified basePackages with Spring Cloud Bus.</p>
+ * <p>This configuration class registers all custom events from current package with Spring Cloud Bus.</p>
  * Created on 12/02/19
  *
  * @author Alexandra Guironnet
  */
 @Configuration
-@RemoteApplicationEventScan(basePackages = "org.lfenergy.operatorfabric.springtools.config.oauth")
+@RemoteApplicationEventScan
 public class BusConfiguration {
 
 }
