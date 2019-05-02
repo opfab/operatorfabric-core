f0f5d2ab13eff43990fde3d2876a178a897c331b
David Binder <david.binder@rte-france.com>
2019-05-02 11:15:56 +0200

    [travis] better handling of docker pushes. (ci_docker)

--- bin/travis/upload_doc.sh
+++ bin/travis/upload_doc.sh
@@ -8,7 +8,7 @@ HTTP_REPO="https://davidbinderRTE:${GH_DOC_TOKEN}@${GH_REPO}"
 git clone $HTTP_REPO $HOME/documentation
 version=$(echo "$OF_VERSION"| sed s/-SNAPSHOT//)
 cd $OF_HOME
-#./gradlew generateSwaggerCodeDoc javadoc asciidoctor :ui:main:npm_run_compodoc
+./gradlew generateSwaggerCodeDoc javadoc asciidoctor :ui:main:npm_run_compodoc
 for prj in "${OF_REL_COMPONENTS[@]}"; do
   echo "copying $prj documentation"
   mkdir -p $HOME/documentation/projects/$prj/$version/
@@ -25,7 +25,7 @@ cd $HOME/documentation
 if [ -n "$(git status --porcelain)" ]; then
     echo "Changes to documentation detected, preparing commit"
     git add .
-    git commit -m "Updating documentation"
+    git commit -m "Updating documentation for version ${OF_VERSION}"
     git push $HTTP_REPO master > /dev/null 2>&1
 else
     echo "No changes to documentation detected"
