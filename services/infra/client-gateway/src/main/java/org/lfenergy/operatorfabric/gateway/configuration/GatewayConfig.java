cee020569921c54ddbe0bbddb08c8751cbda9b5f
David Binder <david.binder@rte-france.com>
2019-05-02 15:43:35 +0200

    [config][OC-251] basic confiuration of client in web-ui.yml + adding response rewrite in gateway to avoid unwanted configuration exposure #time 1h #done

--- services/infra/client-gateway/src/main/java/org/lfenergy/operatorfabric/gateway/configuration/GatewayConfig.java
+++ services/infra/client-gateway/src/main/java/org/lfenergy/operatorfabric/gateway/configuration/GatewayConfig.java
@@ -7,12 +7,17 @@
 
 package org.lfenergy.operatorfabric.gateway.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 /**
  * TODO can't remember the reason of this code, explore and remove if not needed
@@ -22,6 +27,8 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class GatewayConfig {
 
+    ObjectMapper om = new ObjectMapper();
+
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter() {
@@ -30,5 +37,28 @@ public class GatewayConfig {
                 return chain.filter(exchange);
             }
         };
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("config",
+                        r -> r.path("/config")
+                        .filters(f -> {
+                            return f
+                                    .rewritePath("/config", "/web-ui.json")
+                                    .modifyResponseBody(String.class, String.class,
+                                            (exchange, s) -> {
+                                                try {
+                                                    Map map = om.readValue(s, Map.class);
+                                                    return Mono.just(om.writeValueAsString(map.get("operatorfabric")));
+                                                } catch (Exception ex) {
+                                                    return Mono.just(s);
+                                                }
+                                            })
+                                    ;
+                        })
+                        .uri("lb://CONFIG")
+                ).build();
     }
 }
