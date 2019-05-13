79c27899b04966d00ede3b83944671017e0e577a
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-05-13 18:31:13 +0200

    [OC-134] iframe service doesn't need an iframeURL property

--- ui/main/src/app/modules/thirdparty/iframe.service.ts
+++ ui/main/src/app/modules/thirdparty/iframe.service.ts
@@ -6,14 +6,11 @@ import {ReplaySubject} from "rxjs";
 })
 export class IframeService {
 
-  iframeURL : string = "";
-
   @Output() urlUpdate: ReplaySubject<string> = new ReplaySubject(1);
   //Using ReplaySubject because the first urlUpdate is emitted before the component can subscribe to it (when it is initialized).
 
   selectURL(url){
-    this.iframeURL = url;
-    this.urlUpdate.next(this.iframeURL);
+    this.urlUpdate.next(url);
   }
 
 }
