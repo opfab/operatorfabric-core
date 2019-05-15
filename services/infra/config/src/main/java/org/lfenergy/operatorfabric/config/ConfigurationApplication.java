540d55d4ab9b2d5d392929e2c9a7ec78543022f1
David Binder <david.binder@rte-france.com>
2019-05-15 13:56:41 +0200

    [Generic][OC-177] Configure keycloak on server side #time 4h #done

--- services/infra/config/src/main/java/org/lfenergy/operatorfabric/config/ConfigurationApplication.java
+++ services/infra/config/src/main/java/org/lfenergy/operatorfabric/config/ConfigurationApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
