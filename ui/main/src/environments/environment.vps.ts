0938d62c6c9d85f715e15e83526ee4ae86188e35
David Binder <david.binder@rte-france.com>
2019-05-20 15:27:46 +0200

    [UI] add vps configuration

---
+++ ui/main/src/environments/environment.vps.ts
@@ -0,0 +1,30 @@
+/* Copyright (c) 2018, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    urls: {
+      authentication: '',
+        auth: 'http://opfab.rte-europe.com:2002/auth',
+        cards: 'http://opfab.rte-europe.com:2002/cards',
+        archives : '',
+        thirds: 'http://opfab.rte-europe.com:2002/thirds',
+        config: 'http://opfab.rte-europe.com:2002/config'
+    }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
