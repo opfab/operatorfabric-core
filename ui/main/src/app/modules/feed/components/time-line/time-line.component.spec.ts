5ecbf12c4a07d3297a768e98a71c1a6a06b1a485
BaptJulien <baptistejulien92@hotmail.fr>
2019-05-29 10:17:42 +0200

    clean import useless

--- ui/main/src/app/modules/feed/components/time-line/time-line.component.spec.ts
+++ ui/main/src/app/modules/feed/components/time-line/time-line.component.spec.ts
@@ -20,9 +20,6 @@ import {CustomTimelineChartComponent} from "../custom-timeline-chart/custom-time
 import {InitChartComponent} from "../init-chart/init-chart.component";
 import {CustomRouterStateSerializer} from "@ofStates/router.state";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
-import * as moment from 'moment';
-import * as _ from 'lodash';
-import {getOneRandomLigthCard} from "@tests/helpers";
 
 describe('TimeLineComponent', () => {
   let component: TimeLineComponent;
