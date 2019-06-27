d2f33de6cd5a40277b40d6af7ef3058de59e1f7e
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-06-27 13:01:15 +0200

    Fixed Sonar code smell

--- services/core/users/src/main/java/org/lfenergy/operatorfabric/users/configuration/DataInitComponent.java
+++ services/core/users/src/main/java/org/lfenergy/operatorfabric/users/configuration/DataInitComponent.java
@@ -33,7 +33,7 @@ import java.util.Optional;
 @Slf4j
 public class DataInitComponent {
 
-    private final String FAILED_INIT_MESSAGE = "Unable to init ";
+    private static final String failedInitMessage = "Unable to init ";
     @Autowired
     private UsersProperties usersProperties;
 
@@ -77,7 +77,7 @@ public class DataInitComponent {
         try {
             userSettingsRepository.insert(u);
         } catch (DuplicateKeyException ex) {
-            log.warn(FAILED_INIT_MESSAGE + u.getLogin() + " user settings: duplicate");
+            log.warn(failedInitMessage + u.getLogin() + " user settings: duplicate");
         }
     }
 
@@ -92,7 +92,7 @@ public class DataInitComponent {
         try {
             userRepository.insert(u);
         } catch (DuplicateKeyException ex) {
-            log.warn(FAILED_INIT_MESSAGE + u.getLogin() + " user: duplicate");
+            log.warn(failedInitMessage + u.getLogin() + " user: duplicate");
             Optional<UserData> resultUser = userRepository.findById(u.getLogin());
             if (resultUser.isPresent()) {
                 UserData loadedUser = resultUser.get();
@@ -120,7 +120,7 @@ public class DataInitComponent {
         try {
             groupRepository.insert(g);
         } catch (DuplicateKeyException ex) {
-            log.warn(FAILED_INIT_MESSAGE + g.getName() + " group: duplicate");
+            log.warn(failedInitMessage + g.getName() + " group: duplicate");
         }
     }
 }
