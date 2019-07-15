7d5a0974527c5133f511712a7d44569dceab29bd
LE-GALL Ronan Ext <ronan.le-gall@rte-france.com>
2019-07-15 18:13:21 +0200

    [UI] uses mock to test Third Actions

--- ui/main/src/app/modules/cards/components/action/action.component.ts
+++ ui/main/src/app/modules/cards/components/action/action.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Action} from "@ofModel/thirds.model";
 
 @Component({
   selector: 'of-action',
@@ -7,10 +9,25 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ActionComponent implements OnInit {
 
-  @Input() readonly name
-  constructor() { }
+  @Input() readonly action:Action;
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    const protocol = 'http://';
+    const domain = 'localhost';
+    const port = '8080';
+    const instanceProcessId='TEST_process1';
+    const state = 'firstState';
+    const actionId = 'action1';
+    const resource = `/process/${instanceProcessId}/states/${state}/actions/${actionId}`
+    const url = `${protocol}${domain}:${port}/soapui-mock/test-process`;
+
+this.httpClient.post(url
+,this.action
+    ).subscribe();
   }
 
 }
