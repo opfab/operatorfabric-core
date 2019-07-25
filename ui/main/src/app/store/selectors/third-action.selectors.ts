6298d5c3faf8b81efeb558a357bd5cf560b99458
LE-GALL Ronan Ext <ronan.le-gall@rte-france.com>
2019-07-25 11:33:39 +0200

    [UI] removes commented code

--- ui/main/src/app/store/selectors/third-action.selectors.ts
+++ ui/main/src/app/store/selectors/third-action.selectors.ts
@@ -2,18 +2,6 @@ import {ThirdActionState} from "@ofStates/third-action.state";
 import {createFeatureSelector, createSelector} from "@ngrx/store";
 import {LightCard} from "@ofModel/light-card.model";
 
-// const selectThirdActions = (state:AppState) => state.thirdActions;
-//
-// export const selectThirdActionList = createSelector(
-//     selectThirdActions,
-//     (state:ThirdActionState) => state.thirdActionHolder
-// );
-//
-// export const fetchSelectedThirdActionHolder = createSelector(
-//     selectThirdActions,
-//
-// )
-
 export const getThirdActionHolderSelector =createFeatureSelector<ThirdActionState>('thirdActions');
 
 export const selectThirdActionList = createSelector(
