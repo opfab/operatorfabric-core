6f5939a0dcc8d9b7f2c267288db8e21d1e5f82f4
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-08-06 10:06:53 +0200

    [OC-44] Adding tag filter #time 15m

--- ui/main/src/app/modules/archives/components/archive-list/filters/archive-filters.component.ts
+++ ui/main/src/app/modules/archives/components/archive-list/filters/archive-filters.component.ts
@@ -10,9 +10,9 @@ import {Observable} from "rxjs";
 import {Store} from "@ngrx/store";
 import {AppState} from "@ofStore/index";
 import {buildConfigSelector} from "@ofSelectors/config.selectors";
-import {TryToLogIn} from "@ofActions/authentication.actions";
 import {SendArchiveQuery} from "@ofActions/archive.actions";
 import {selectArchiveFilters} from "@ofSelectors/archive.selectors";
+import {ThirdsService} from "@ofServices/thirds.service";
 
 @Component({
   selector: 'of-archive-filters',
@@ -23,6 +23,7 @@ export class ArchiveFiltersComponent implements OnInit {
 
   publishers$:Observable<string []>;
   processes$:Observable<string []>;
+  tags$:Observable<string []>; //TODO Not necessarily strings, could be value/ labels or i18n but it doesn't seem to be a problem
 
   filters: Map<string,string[]>;
 
@@ -32,6 +33,7 @@ export class ArchiveFiltersComponent implements OnInit {
   ngOnInit() {
     this.publishers$ = this.store.select(buildConfigSelector('archive.filters.publisher.list'));
     this.processes$ = this.store.select(buildConfigSelector('archive.filters.process.list'));
+    this.tags$ = this.store.select(buildConfigSelector('archive.filters.tags.list'));
 
     this.store.select(selectArchiveFilters).subscribe( next => this.filters = next);
   }
