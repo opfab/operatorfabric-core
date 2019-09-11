060052d33b800d1ae989c6233d45f0499bed59c0
Amine TABOU <amin.tabou@gmail.com>
2019-09-11 09:45:38 +0200

    [OC-358] describe instead of fdescribe

--- ui/main/src/app/components/icon/icon.component.spec.ts
+++ ui/main/src/app/components/icon/icon.component.spec.ts
@@ -9,7 +9,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {IconComponent} from './icon.component';
 
-fdescribe('IconComponent', () => {
+describe('IconComponent', () => {
   let component: IconComponent;
   let fixture: ComponentFixture<IconComponent>;
 
