4405c47842d997ad70245e4a4163aea63e10b962
Amine TABOU <amin.tabou@gmail.com>
2019-09-19 17:58:23 +0200

    [OC-440] Visibility of the tags in settings page

--- ui/main/src/app/modules/settings/components/settings/settings.component.ts
+++ ui/main/src/app/modules/settings/components/settings/settings.component.ts
@@ -6,25 +6,26 @@
  */
 
 import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
-import {Store} from "@ngrx/store";
-import {AppState} from "@ofStore/index";
-import {buildConfigSelector} from "@ofSelectors/config.selectors";
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {AppState} from '@ofStore/index';
+import {buildConfigSelector} from '@ofSelectors/config.selectors';
 
 @Component({
   selector: 'of-settings',
   templateUrl: './settings.component.html'
 })
 export class SettingsComponent implements OnInit {
-  locales$:Observable<string[]>;
+  locales$: Observable<string[]>;
   timeZones$: Observable<string[]>;
+  hideTags$: Observable<boolean>;
 
-  constructor(private store:Store<AppState>) { }
-
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.locales$ = this.store.select(buildConfigSelector('i18n.supported.locales'));
     this.timeZones$ = this.store.select(buildConfigSelector('i10n.supported.time-zones'));
+    this.hideTags$ = this.store.select(buildConfigSelector('settings.hideTags'));
   }
 
 }
