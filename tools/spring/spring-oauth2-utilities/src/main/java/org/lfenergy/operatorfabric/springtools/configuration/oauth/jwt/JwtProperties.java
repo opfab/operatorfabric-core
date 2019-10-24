10fe63956052682248c4b79ceed752798ff866e6
You-Liang CHENG <you-liang.cheng@rte-france.com>
2019-10-24 10:49:25 +0200

    [OC-27] set default values for mandatories properties

--- tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/jwt/JwtProperties.java
+++ tools/spring/spring-oauth2-utilities/src/main/java/org/lfenergy/operatorfabric/springtools/configuration/oauth/jwt/JwtProperties.java
@@ -1,5 +1,9 @@
 package org.lfenergy.operatorfabric.springtools.configuration.oauth.jwt;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +20,10 @@ import lombok.Data;
 @Data
 public class JwtProperties {
 	
-	public String subClaim;
+	// mandatory claim, default value
+	public String subClaim = "sub";
+	
+	// optional claims
 	public String givenNameClaim;
 	public String familyNameClaim;
 
