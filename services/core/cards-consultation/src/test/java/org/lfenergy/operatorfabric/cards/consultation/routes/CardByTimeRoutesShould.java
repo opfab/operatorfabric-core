2f7e00bcde6b56031de3857f6123cb5b3111eeb0
LE-GALL Ronan Ext <ronan.le-gall@rte-france.com>
2019-10-25 17:59:48 +0200

    [OC-371] one card in the future is push in the repo

--- services/core/cards-consultation/src/test/java/org/lfenergy/operatorfabric/cards/consultation/routes/CardByTimeRoutesShould.java
+++ services/core/cards-consultation/src/test/java/org/lfenergy/operatorfabric/cards/consultation/routes/CardByTimeRoutesShould.java
@@ -9,8 +9,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.lfenergy.operatorfabric.cards.consultation.application.IntegrationTestApplication;
 import org.lfenergy.operatorfabric.cards.consultation.configuration.webflux.CardByTimeRoutesConfig;
 import org.lfenergy.operatorfabric.cards.consultation.model.CardConsultationData;
+import org.lfenergy.operatorfabric.cards.consultation.model.I18nConsultationData;
+import org.lfenergy.operatorfabric.cards.consultation.model.Recipient;
+import org.lfenergy.operatorfabric.cards.consultation.model.RecipientConsultationData;
 import org.lfenergy.operatorfabric.cards.consultation.repositories.CardRepository;
+import org.lfenergy.operatorfabric.cards.model.RecipientEnum;
+import org.lfenergy.operatorfabric.cards.model.SeverityEnum;
 import org.lfenergy.operatorfabric.springtools.configuration.test.WithMockOpFabUser;
+import org.lfenergy.operatorfabric.users.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +25,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.test.StepVerifier;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -67,19 +74,35 @@ public class CardByTimeRoutesShould {
             webTestClient.get().uri("/{millisTime}/next",System.currentTimeMillis()).exchange()
                     .expectStatus().isNotFound();
         }
-
+        @Test
         public void findNextCard_ForCurrentUser(){
             Instant now = Instant.now();
             Instant inOneHour = now.plus(1,ChronoUnit.HOURS);
-            CardConsultationData cardInFutureForCurrentUser = CardConsultationData
+
+            Recipient userAsRecipient = RecipientConsultationData.builder()
+                    .type(RecipientEnum.USER)
+                    .identity("userWithGroup")
+                    .build();
+            CardConsultationData cardForUserStartingInOnHourFromNow = CardConsultationData
                     .builder()
                     .publisher("total")
                     .publisherVersion("the most recent")
                     .processId("turlututu-chapeau-pointu")
                     .process("current-proocess")
+                    .title(new I18nConsultationData())
+                    .summary(new I18nConsultationData())
+                    .severity(SeverityEnum.ACTION)
                     .publishDate(now)
                     .startDate(inOneHour)
+                    .recipient(userAsRecipient)
                     .build();
+
+            StepVerifier.create(repository.save(cardForUserStartingInOnHourFromNow))
+                    .expectNextCount(1)
+                    .expectComplete()
+                    .verify();
+
+            // verify that card is found…
 
         }
 
