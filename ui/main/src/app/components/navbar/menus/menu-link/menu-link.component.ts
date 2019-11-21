5395d76bfb960b636bbded92c1e00d676bbc78f8
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-11-21 16:05:48 +0100

    [OC-524] Make link type configurable

--- ui/main/src/app/components/navbar/menus/menu-link/menu-link.component.ts
+++ ui/main/src/app/components/navbar/menus/menu-link/menu-link.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {LightCard} from "@ofModel/light-card.model";
 import {ThirdMenu, ThirdMenuEntry} from "@ofModel/thirds.model";
+import {buildConfigSelector} from "@ofSelectors/config.selectors";
+import {Store} from "@ngrx/store";
+import {AppState} from "@ofStore/index";
 
 @Component({
   selector: 'of-menu-link',
@@ -11,10 +13,28 @@ export class MenuLinkComponent implements OnInit {
 
   @Input() public menu: ThirdMenu;
   @Input() public menuEntry: ThirdMenuEntry;
+  menusOpenInTabs: boolean;
+  menusOpenInIframes: boolean;
+  menusOpenInBoth: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<AppState>) {
   }
 
+  ngOnInit() {
+    this.store.select(buildConfigSelector('navbar.thirdmenus.type', 'BOTH'))
+        .subscribe(v=> {
+          if(v == 'TAB') {
+            this.menusOpenInTabs = true;
+          } else if (v == 'IFRAME') {
+            this.menusOpenInIframes = true;
+          } else {
+            if (!(v == 'BOTH')) {
+              console.log("MenuLinkComponent - Property navbar.thirdmenus.type has an unexpected value: "+v+". Default (BOTH) will be applied.")
+            }
+            this.menusOpenInBoth = true;
+          }
+        })
+  }
 }
+
+
