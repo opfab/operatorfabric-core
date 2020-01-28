1110bbbe82081d8bd9997ad06581fbb8913a9b30
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-28 14:40:39 +0100

    [OC-625] Removed 2018 headers

--- ui/main/src/app/store/reducers/archive.reducer.spec.ts
+++ ui/main/src/app/store/reducers/archive.reducer.spec.ts
@@ -1,9 +1,3 @@
-/* Copyright (c) 2018, RTE (http://www.rte-france.com)
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
 import { reducer} from './archive.reducer';
 import { UpdateArchiveFilter } from '@ofStore/actions/archive.actions';
 import { archiveInitialState } from '@ofStore/states/archive.state';
--- ui/main/src/polyfills.ts
+++ ui/main/src/polyfills.ts
@@ -1,9 +1,3 @@
-/* Copyright (c) 2018, RTE (http://www.rte-france.com)
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
 
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
