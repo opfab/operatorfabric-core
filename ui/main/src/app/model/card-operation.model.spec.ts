cb6374115fffada72df1c2250efd3720d418cf6d
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-28 21:29:28 +0100

    [OC-625] Added 2020 headers

--- ui/main/src/app/model/card-operation.model.spec.ts
+++ ui/main/src/app/model/card-operation.model.spec.ts
@@ -1,3 +1,10 @@
+/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {CardOperation, CardOperationType} from "@ofModel/card-operation.model";
 import {getRandomAlphanumericValue} from "@tests/helpers";
 
