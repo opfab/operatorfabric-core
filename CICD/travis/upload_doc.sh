2aff65d8042c2b33d9eedb3fb43b62093688b18b
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-02-25 17:07:01 +0100

    [Doc] Removing extra generation of doc ci_documentation

--- CICD/travis/upload_doc.sh
+++ CICD/travis/upload_doc.sh
@@ -7,19 +7,9 @@ GH_REPO=github.com/opfab/opfab.github.io.git
 HTTP_REPO="https://opfabtech:${GH_DOC_TOKEN}@${GH_REPO}"
 git clone $HTTP_REPO $HOME/documentation
 
-cd $HOME/documentation
-echo "List files after inital clone"
-LIST_FILES=$(ls $HOME/documentation)
-echo $LIST_FILES
-GIT_CONFIG=$(git config --list --show-origin)
-echo $GIT_CONFIG
-cd $CURRENT_PATH
-
 # TODO Find out what sed below is for (as we don't have - in our version tags)
 version=$(echo "$OF_VERSION"| sed s/-SNAPSHOT//)
 cd $OF_HOME
-# TODO It looks like doc generation is already done in travis.yml To be checked
-./gradlew generateSwaggerCodeDoc javadoc asciidoctor :ui:main:npm_run_compodoc
 for prj in "${OF_REL_COMPONENTS[@]}"; do
   echo "copying $prj documentation"
   rm -r $HOME/documentation/projects/$prj/$version/*
@@ -35,10 +25,6 @@ cp -r $OF_HOME/ui/main/documentation/* $HOME/documentation/projects/ui/main/$ver
 cp -r $OF_HOME/ui/main/reports/* $HOME/documentation/projects/ui/main/$version/reports/.
 cp -r $OF_HOME/build/asciidoc/html5/* $HOME/documentation/documentation/$version/.
 cd $HOME/documentation
-
-echo "Results of copy"
-COPY_RES=$(git status)
-echo $COPY_RES
 
 if [ -n "$(git status --porcelain)" ]; then
     echo "Changes to documentation detected, preparing commit"
