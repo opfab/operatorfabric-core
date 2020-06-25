244385c5d1cc87a85877acc2fd2a096fd222c9a1
freddidierRTE <frederic-f.didier@rte-france.com>
2020-06-25 15:21:08 +0200

    [OC-974] Redraw card when switching day/night mode

---
+++ ui/main/src/app/store/actions/global-style.actions.ts
@@ -0,0 +1,30 @@
+/* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+
+import {Action} from '@ngrx/store';
+
+export enum GlobalStyleActionTypes {
+    GlobalStyleUpdate = '[Style] Style update'
+}
+
+export class GlobalStyleUpdate implements Action {
+    readonly type = GlobalStyleActionTypes.GlobalStyleUpdate;
+    constructor(public payload : {style : string}) {}
+}
+
+
+export type GlobalStyleActions = GlobalStyleUpdate;
