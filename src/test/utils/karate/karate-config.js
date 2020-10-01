805ad05c75d48b903dd31d153893d26ad470414c
freddidierRTE <frederic-f.didier@rte-france.com>
2020-10-01 16:05:40 +0200

    Move license headers for karate conf files

--- src/test/utils/karate/karate-config.js
+++ src/test/utils/karate/karate-config.js
@@ -1,4 +1,7 @@
-/* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+
+function fn() {   
+
+  /* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -6,9 +9,6 @@
  * SPDX-License-Identifier: MPL-2.0
  * This file is part of the OperatorFabric project.
  */
-
-function fn() {   
-
     
     var config = { // base config JSON
       opfabUrl: 'http://localhost:2002/',
