e4650d8699a681d2037d43205b109ac7d5544c0e
freddidierRTE <frederic-f.didier@rte-france.com>
2020-10-01 15:37:27 +0200

    Correct license headers

--- ui/main/src/app/store/actions/monitoring.actions.ts
+++ ui/main/src/app/store/actions/monitoring.actions.ts
@@ -1,3 +1,12 @@
+/* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
 import {Action} from '@ngrx/store';
 import {Page} from '@ofModel/page.model';
 import {LineOfMonitoringResult} from '@ofModel/line-of-monitoring-result.model';
