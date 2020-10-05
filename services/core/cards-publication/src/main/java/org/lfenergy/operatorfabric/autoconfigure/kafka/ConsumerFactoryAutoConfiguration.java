8239a7713b9a9fad7acb21d97ce1354ea43bcc19
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2020-10-05 11:15:23 +0200

    [OC-1073-serialization] removed redundant configuration code
    Signed-off-by: Sjoerd Adema <sjoerd@kristalsoftware.nl>

--- services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/autoconfigure/kafka/ConsumerFactoryAutoConfiguration.java
+++ services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/autoconfigure/kafka/ConsumerFactoryAutoConfiguration.java
@@ -3,11 +3,9 @@ package org.lfenergy.operatorfabric.autoconfigure.kafka;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.lfenergy.operatorfabric.avro.CardCommand;
 import org.lfenergy.operatorfabric.cards.publication.kafka.consumer.CardCommandConsumerDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -26,18 +24,9 @@ public class ConsumerFactoryAutoConfiguration {
 
     private final KafkaProperties kafkaProperties;
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
-
     private Map<String,Object> consumerConfig() {
         Map<String,Object> props = kafkaProperties.buildConsumerProperties();
         log.info("bootstrapServers: " + kafkaProperties.getBootstrapServers());
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
     }
 
