47f94983f6accc74a832ef1fa428e220b0248af8
freddidierRTE <frederic-f.didier@rte-france.com>
2020-10-08 15:07:58 +0200

    [OC-1157] Colors modifications for day mode
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/modules/cards/components/detail/detail.component.ts
+++ ui/main/src/app/modules/cards/components/detail/detail.component.ts
@@ -83,7 +83,8 @@ const enum EntityMsgColor {
 
 @Component({
     selector: 'of-detail',
-    templateUrl: './detail.component.html'
+    templateUrl: './detail.component.html',
+    styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnChanges, OnInit, OnDestroy, AfterViewChecked,DoCheck {
 
