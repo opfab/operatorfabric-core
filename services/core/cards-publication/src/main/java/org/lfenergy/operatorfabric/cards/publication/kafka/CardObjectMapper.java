9b54e90116a2c4e33c0afa0d6c72bacbae24ad12
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2020-10-20 13:40:26 +0200

    [OC-1073-esm-207] register modules for objectmapper
    Signed-off-by: Sjoerd Adema <sjoerd@kristalsoftware.nl>

--- services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/cards/publication/kafka/CardObjectMapper.java
+++ services/core/cards-publication/src/main/java/org/lfenergy/operatorfabric/cards/publication/kafka/CardObjectMapper.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.lfenergy.operatorfabric.avro.Card;
+import org.lfenergy.operatorfabric.cards.publication.configuration.json.CardsModule;
 import org.lfenergy.operatorfabric.cards.publication.model.CardPublicationData;
+import org.lfenergy.operatorfabric.springtools.json.InstantModule;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -18,6 +22,10 @@ public class CardObjectMapper {
     public CardObjectMapper() {
         this.objectMapper = new ObjectMapper();
         objectMapper.enable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS);
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new CardsModule());
+        objectMapper.registerModule(new InstantModule());
     }
 
     public String writeValueAsString(Card kafkaCard) throws JsonProcessingException {
