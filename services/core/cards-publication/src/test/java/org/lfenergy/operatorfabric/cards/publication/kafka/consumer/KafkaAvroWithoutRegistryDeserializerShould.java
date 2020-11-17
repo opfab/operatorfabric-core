feef637ddb367ad3497013ae18ec17d1d0f9d6a0
Jeroen Gommans <jeroen@anjersoftwaredesign.nl>
2020-11-17 09:16:49 +0100

    Updated copyright, added unit tests
    Signed-off-by: Jeroen Gommans <jeroen@anjersoftwaredesign.nl>

--- services/core/cards-publication/src/test/java/org/lfenergy/operatorfabric/cards/publication/kafka/consumer/KafkaAvroWithoutRegistryDeserializerShould.java
+++ services/core/cards-publication/src/test/java/org/lfenergy/operatorfabric/cards/publication/kafka/consumer/KafkaAvroWithoutRegistryDeserializerShould.java
@@ -1,4 +1,11 @@
-package org.lfenergy.operatorfabric.cards.publication.kafka.consumer;
+/* Copyright (c) 2020, Alliander (http://www.alliander.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */package org.lfenergy.operatorfabric.cards.publication.kafka.consumer;
 
 import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.DatumReader;
--- services/core/cards-publication/src/test/java/org/lfenergy/operatorfabric/cards/publication/kafka/producer/KafkaAvroWithoutRegistrySerializerShould.java
+++ services/core/cards-publication/src/test/java/org/lfenergy/operatorfabric/cards/publication/kafka/producer/KafkaAvroWithoutRegistrySerializerShould.java
@@ -1,3 +1,11 @@
+/* Copyright (c) 2020, Alliander (http://www.alliander.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
 package org.lfenergy.operatorfabric.cards.publication.kafka.producer;
 
 import org.apache.kafka.common.errors.SerializationException;
