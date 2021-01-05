525b87b98ce21e6cdc16fc4962108d3c8e671dd9
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2021-01-05 13:17:10 +0100

    [OC-1283] Allow external config location for docker-compose and making run_all work on mac

--- config/docker/docker-compose.sh
+++ config/docker/docker-compose.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+echo "Usage: "
+echo "   1: ./docker-compose.sh"
+echo "   2: ./docker-compose.sh <pathToExternalEnvironmentFile>"
+
 echo USER_ID="$(id -u)" > .env
 echo USER_GID="$(id -g)" >> .env
 # create directory for bundle storage if not existing
 mkdir -p businessconfig-storage
+if [ "$#" -eq 0 ]; then
+  echo CONFIG_PATH=./ >> .env
+  echo SPRING_PROFILES_ACTIVE=docker >> .env
+else
+  EXTERNAL_CONFIGURATION_FILE=$1
+  CONFIG_PATH=$(dirname "$1")
+  cat ${EXTERNAL_CONFIGURATION_FILE} >> .env
+  echo CONFIG_PATH="$CONFIG_PATH" >> .env
+fi
+cat .env
 docker-compose up -d
