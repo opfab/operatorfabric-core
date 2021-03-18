0d53832ef966450c0933a6d1d68869222a30cc98
freddidierRTE <frederic-f.didier@rte-france.com>
2021-03-18 16:44:01 +0100

    [OC-1571] Create a repository with common bundles
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/utils/karate/Action/packageBundles.sh
+++
@@ -1,4 +0,0 @@
-cd ../../../api/karate/businessconfig/resources/bundle_test_action/
-tar -czvf bundle_test_action.tar.gz config.json css/ template/ i18n/
-mv bundle_test_action.tar.gz ../../../../../utils/karate/Action/
-cd -
