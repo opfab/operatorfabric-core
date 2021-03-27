6651076fe4866a15ae34e3a94da4e3df4c788897
freddidierRTE <frederic-f.didier@rte-france.com>
2021-03-27 18:47:45 +0100

    [OC-1597] Improve communication between template and opfab
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/assets/js/templateGateway.js
+++ ui/main/src/assets/js/templateGateway.js
@@ -11,6 +11,7 @@
 const templateGateway = {
     opfabEntityNames : null, 
     childCards: [],
+    userAllowedToRespond : false,
 
     setEntityNames: function(entityNames){
         this.opfabEntityNames = entityNames;
@@ -38,7 +39,13 @@ const templateGateway = {
         document.location.href = newUrl;
     },
 
-
+    // True if user is allowed to respond to the current card :
+    //  - his entity is allowed to respond 
+    //  - he is member of a group having a perimeter permitting the response 
+    isUserAllowedToRespond  : function() {
+        return this.userAllowedToRespond;
+    },
+    
     //
     // FUNCTIONS TO OVERRIDE BY TEMPLATES 
     //
@@ -59,13 +66,9 @@ const templateGateway = {
     // size = 'lg' for large size , i.e when the card is in full screen mode
     setScreenSize: function(size) {},
 
-    // Opfab call this method to tell the template if the user can respond to the card
-    // responseEnabled set to true if possible , false otherwise 
-    setUserCanRespond: function(responseEnabled) {},
-
     // Opfab call this method to get the form result when user want to send response 
-    validyForm: function() {
-        console.log(new Date().toISOString() , ` Template.js : no validyForm method define in template , valid set to true`);
+    getUserResponse: function() {
+        console.log(new Date().toISOString() , ` Template.js : no getUserResponse method define in template , valid set to true`);
         return this.isValid = undefined;
     }
     
