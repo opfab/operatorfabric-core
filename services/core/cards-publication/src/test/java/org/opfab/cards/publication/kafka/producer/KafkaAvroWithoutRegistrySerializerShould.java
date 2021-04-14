c9de1f93cc80f7d2a809ce7a8b373701cf1be09b
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2021-04-14 06:48:18 +0200

    [OC-1650] Copyright updates
    Signed-off-by: Alexandra Guironnet <alexandra.guironnet@rte-france.com>

--- services/core/cards-publication/src/test/java/org/opfab/cards/publication/kafka/producer/KafkaAvroWithoutRegistrySerializerShould.java
+++ services/core/cards-publication/src/test/java/org/opfab/cards/publication/kafka/producer/KafkaAvroWithoutRegistrySerializerShould.java
@@ -1,4 +1,5 @@
 /* Copyright (c) 2020, Alliander (http://www.alliander.com)
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
