4d6b708258439c6ccf80baa7a3dab1629152bfc5
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2021-04-21 13:43:38 +0200

    [OC-1564][OC-1565] Integrating cypress tests to Gradle build and CI

--- src/test/api/karate/buildAndLaunchAll.sh
+++ src/test/api/karate/buildAndLaunchAll.sh
@@ -31,8 +31,9 @@ echo "Start opfab"
 cd config/docker
 ./docker-compose.sh
 echo "Starting in progress..."
-cd ../../src/test/api/karate
+cd ../../src/test/resources
 ./waitForOpfabToStart.sh
+cd ../api/karate
 echo "Start karate testing"
 ./launchAll.sh
 google-chrome target/karate-reports/karate-summary.html &
