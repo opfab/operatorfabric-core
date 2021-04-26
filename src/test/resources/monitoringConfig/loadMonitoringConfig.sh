8506b06741467adf8732e67198c293bba8deb83d
freddidierRTE <frederic-f.didier@rte-france.com>
2021-04-26 17:56:06 +0200

    [OC-1442] Monitoring export - Add monitoring config example
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ src/test/resources/monitoringConfig/loadMonitoringConfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Copyright (c) 2021, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of the OperatorFabric project.
+
+# This starts by moving to the directory where the script is located so the paths below still work even if the script
+# is called from another folder
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+url=$2 
+if [ -z $url ] 
+then
+	url="http://localhost"
+fi
+if [ -z $1 ]
+then
+    echo "Usage loadMonitoringConfig file_name opfab_url"
+else
+	echo "Will load monitoringConfig $1 on $url"
+	source ../getToken.sh $url
+	curl -s -v -X POST "$url:2100/businessconfig/monitoring" -H  "accept: application/json" -H "Authorization:Bearer $token" -H "Content-type:application/json" --data @$1
+	echo ""
+fi
+
+
