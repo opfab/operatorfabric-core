4dd1b2f8b284d0c7165715114ed3e733a0a486d0
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2021-04-29 12:06:29 +0200

    [OC-1681] Changes to Cypress tests following discussion

--- src/test/cypress/cypress/support/commands.js
+++ src/test/cypress/cypress/support/commands.js
@@ -1,72 +1,9 @@
-//TODO Check that all commands are used
-
-const ONE_SECOND = 1000;
 
 Cypress.Commands.add('waitDefaultTime', () => {
     cy.wait(Cypress.env('defaultWaitTime'))
 })
 
-Cypress.Commands.add('PushCard', (processName, processVersion, publisherName, processId, message, severity, startDate, endDate)=>
-{
-    cy.request({
-        method : 'POST',
-        url : Cypress.env('host')+':2102/cards',
-        body: {
-            "publisher" : publisherName,
-            "processVersion" : processVersion,
-            "process"  : processName,
-            "processInstanceId" : processId,
-            "state": message,
-            "groupRecipients": ["Dispatcher","ADMIN"],
-            "severity" : severity ,
-            "tags": [
-                "MyAwesomeTag","MyTests","Emergency"
-            ],
-            "startDate" : startDate,
-            "endDate" : endDate,
-            "summary" : {"key" : "message.summary"},
-            "title" : {"key" : "message.title"},
-            "data" : {"message":"a message"}
-        }
-    }).then(response =>{
-        cy.expect(response.status).to.eq(201);
-        cy.expect(response.body).to.have.property('count', 1);
-
-    })
-})
-
-Cypress.Commands.add('PushActionCard', (processName, processVersion, publisherName, processId, message, severity, startDate, endDate,lttd)=>
-{
-    cy.request({
-        method : 'POST',
-        url : Cypress.env('host')+':2102/cards',
-        body: {
-            "publisher" : publisherName,
-            "processVersion" : processVersion,
-            "process"  : processName,
-            "processInstanceId" : processId,
-            "state": message,
-            "groupRecipients": ["Dispatcher","Planner","Supervisor"],
-            "entitiesAllowedToRespond": ["ENTITY1","ENTITY2"],
-            "severity" : severity ,
-            "tags": [
-                "MyAwesomeTag","MyTests","Emergency"
-            ],
-            "startDate" : startDate,
-            "endDate" : endDate,
-            "lttd" : lttd,
-            "summary" : {"key" : "message.summary"},
-            "title" : {"key" : "message.title"},
-            "data" : {"message":"a message"}
-        }
-    }).then(response =>{
-        cy.expect(response.status).to.eq(201);
-        cy.expect(response.body).to.have.property('count', 1);
-
-    })
-})
-
-Cypress.Commands.add('LogOpFab',(username, password)=>
+Cypress.Commands.add('loginOpFab',(username, password)=>
 {   //go to login page
     cy.visit('')
 
@@ -84,4 +21,14 @@ Cypress.Commands.add('LogOpFab',(username, password)=>
 
     //Wait for the app to finish initializing
     cy.get('#cypress-loaded-check', {timeout: 10000}).should('have.text', 'true');
+})
+
+Cypress.Commands.add('loadTestConf', () => {
+    // This clears existing processGroups, bundles and perimeters and load the test configuration
+    cy.exec('cd .. && ./resources/loadTestConf.sh '+Cypress.env('host'));
+})
+
+Cypress.Commands.add('sendTestCards', () => {
+    cy.exec('cd .. && ./resources/delete6TestCards.sh '+Cypress.env('host'));
+    cy.exec('cd .. && ./resources/send6TestCards.sh '+Cypress.env('host'));
 })
