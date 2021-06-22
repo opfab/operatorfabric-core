b20c90173c585a056fc33e1f5b5948fbf58bfa29
freddidierRTE <frederic-f.didier@rte-france.com>
2021-06-22 14:35:11 +0200

    [OC-1742] Add a spinner in feed when opfab is loading cards in background
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/modules/feed/components/card-list/filters/filters.component.ts
+++ ui/main/src/app/modules/feed/components/card-list/filters/filters.component.ts
@@ -13,6 +13,7 @@ import {Component, OnInit} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '@ofStore/index';
 import { ConfigService} from "@ofServices/config.service";
+import {LightCardsService} from '@ofServices/lightcards.service';
 
 @Component({
   selector: 'of-filters',
@@ -27,8 +28,10 @@ export class FiltersComponent implements OnInit {
   hideTimerTags: boolean;
   hideReadSort: boolean;
   hideSeveritySort: boolean;
+  
+  loadingInProgress = false;
 
-  constructor(private store: Store<AppState>, private configService: ConfigService) { }
+  constructor(private store: Store<AppState>, private configService: ConfigService, private lightCardsService: LightCardsService) { }
 
   ngOnInit() {
     this.hideTags = this.configService.getConfigValue('settings.tags.hide',false);
@@ -37,6 +40,7 @@ export class FiltersComponent implements OnInit {
     this.hideResponseFilter = this.configService.getConfigValue('feed.card.hideResponseFilter',false);
     this.hideReadSort = this.configService.getConfigValue('feed.card.hideReadSort',false);
     this.hideSeveritySort = this.configService.getConfigValue('feed.card.hideSeveritySort',false);
+    this.lightCardsService.getLoadingInProgress().subscribe( (inProgress: boolean ) => this.loadingInProgress = inProgress)
   }
 
 
