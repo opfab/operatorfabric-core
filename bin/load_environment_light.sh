65d71ffcc26062c0b3e73f61d5ff618191476776
vlo-rte <valerie.longa@rte-france.com>
2021-06-30 18:26:17 +0200

    Fix #1255 Update Angular to v12
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+# Copyright (c) 2018-2021, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,5 +16,5 @@ sdk use gradle 6.9
 # the java version is externalized because also needed for travis configuration
 sdk install java $(<JAVA_VERSION)-zulu
 sdk use java $(<JAVA_VERSION)-zulu
-nvm install v10.16.3
-nvm use v10.16.3
+nvm install v12.14
+nvm use v12.14
