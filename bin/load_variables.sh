7b5c3bcf3d2ecf7dbc60aa65caa01944e5231316
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2021-11-08 22:01:51 +0100

    Removing unused env variables (#1905)
    Signed-off-by: Alexandra Guironnet <alexandra.guironnet@rte-france.com>

--- bin/load_variables.sh
+++ bin/load_variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+# Copyright (c) 2018-2021, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,33 +16,19 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 export OF_HOME=$(realpath $DIR/..)
-export OF_CORE=$OF_HOME/services/core
-export OF_CLIENT=$OF_HOME/client
-export OF_TOOLS=$OF_HOME/tools
-export OF_COMPONENTS=( "$OF_TOOLS/swagger-spring-generators" "$OF_TOOLS/generic/utilities" "$OF_TOOLS/generic/test-utilities"  )
-OF_COMPONENTS+=( "$OF_TOOLS/spring/spring-utilities" "$OF_TOOLS/spring/spring-mongo-utilities" "$OF_TOOLS/spring/aop-process" "$OF_TOOLS/spring/spring-oauth2-utilities" )
-OF_COMPONENTS+=( "$OF_CLIENT/cards" "$OF_CLIENT/users")
-OF_COMPONENTS+=("$OF_CORE/businessconfig" "$OF_CORE/cards-publication" "$OF_CORE/cards-consultation" "$OF_CORE/users")
-
-export OF_REL_COMPONENTS=( "tools/swagger-spring-generators" "tools/generic/utilities" "tools/generic/test-utilities" )
-OF_REL_COMPONENTS+=( "tools/spring/spring-utilities" "tools/spring/spring-mongo-utilities" "tools/spring/aop-process" "tools/spring/spring-oauth2-utilities" )
-OF_REL_COMPONENTS+=( "client/cards" "client/users" "client/businessconfig")
-OF_REL_COMPONENTS+=("services/core/businessconfig" "services/core/cards-publication" "services/core/cards-consultation" "services/core/users" )
 
 export OF_VERSION=$(cat "$OF_HOME/VERSION")
 
 export OF_CLIENT_REL_COMPONENTS=( "cards" "users" "businessconfig")
 
 echo "OPERATORFABRIC ENVIRONMENT VARIABLES"
-
-echo OF_HOME=$OF_HOME
-echo OF_CORE=$OF_CORE
-echo OF_CLIENT=$OF_CLIENT
-echo OF_TOOLS=$OF_TOOLS
 echo
-echo "OperatorFabric version is $OF_VERSION"
+echo OF_HOME=$OF_HOME
+echo OF_VERSION=$OF_VERSION
+
 index=0
-for prj in "${OF_COMPONENTS[@]}"; do
-    echo OF_COMPONENTS[$index]=$prj
+for prj in "${OF_CLIENT_REL_COMPONENTS[@]}"; do
+    echo OF_CLIENT_REL_COMPONENTS[$index]=$prj
     index=$((index + 1))
 done
+
