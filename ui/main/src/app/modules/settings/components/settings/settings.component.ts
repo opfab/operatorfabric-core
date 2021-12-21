79c116d6357b45427a8e41907d41ac1e8baadb2e
vlo-rte <valerie.longa@rte-france.com>
2021-12-21 15:39:27 +0100

    Hide "Play sound on external device" setting if none is configured for the user (#2273)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- ui/main/src/app/modules/settings/components/settings/settings.component.ts
+++ ui/main/src/app/modules/settings/components/settings/settings.component.ts
@@ -13,6 +13,9 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppState} from '@ofStore/index';
 import {ConfigService} from '@ofServices/config.service';
+import {ExternalDevicesService} from "@ofServices/external-devices.service";
+import {UserService} from "@ofServices/user.service";
+import {UserConfiguration} from "@ofModel/external-devices.model";
 
 @Component({
   selector: 'of-settings',
@@ -31,7 +34,12 @@ export class SettingsComponent implements OnInit {
   replayEnabledDefaultValue: boolean;
   replayIntervalDefaultValue: number;
 
-  constructor(private store: Store<AppState>,private  configService: ConfigService) { }
+  userConfiguration: UserConfiguration;
+
+  constructor(private store: Store<AppState>,
+              private configService: ConfigService,
+              private userService: UserService,
+              private externalDevicesService: ExternalDevicesService) { }
 
   ngOnInit() {
     this.locales = this.configService.getConfigValue('i18n.supported.locales');
@@ -44,6 +52,15 @@ export class SettingsComponent implements OnInit {
     this.playSoundForInformationDefaultValue = !!this.configService.getConfigValue('settings.playSoundForInformation') ? this.configService.getConfigValue('settings.playSoundForInformation') : false;
     this.replayEnabledDefaultValue = !!this.configService.getConfigValue('settings.replayEnabled') ? this.configService.getConfigValue('settings.replayEnabled') : false;
     this.replayIntervalDefaultValue = !!this.configService.getConfigValue('settings.replayInterval') ? this.configService.getConfigValue('settings.replayInterval') : 5;
+
+    const userLogin = this.userService.getCurrentUserWithPerimeters().userData.login;
+    this.externalDevicesService.fetchUserConfiguration(userLogin).subscribe(result => {
+      this.userConfiguration = result;
+    });
+  }
+
+  isExternalDeviceConfiguredForUser() : boolean {
+    return (!!this.userConfiguration && !!this.userConfiguration.externalDeviceId);
   }
 
 }
