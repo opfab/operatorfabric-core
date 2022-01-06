cc13447bf7f6b81957f53c03004f06ba244690b3
Berwout de Vries Robles <berwout.de.vries.robles@openvalue.nl>
2022-01-06 11:55:00 +0100

    Update the 'Try it!' scripts to work on macOS out of the box (#2342)
    Signed-off-by: Berwout de Vries Robles <berwout.de.vries.robles@openvalue.nl>

--- src/test/resources/cards/sendCard.sh
+++ src/test/resources/cards/sendCard.sh
@@ -14,20 +14,34 @@ if [ -z $url ]
 then
 	url="http://localhost"
 fi
+
+# MacOs doesn't have date, so check for that and use gdate instead.
+if [ $OSTYPE == 'darwin'* ]
+then
+  if ! command -v gdate &> /dev/null
+  then
+      echo "You are running on macOs and gdate could not be found, please install with 'brew install coreutils'."
+      exit
+  fi
+  current_date_millis=$(gdate +%s%3N)
+else
+  current_date_millis=$(date +%s%3N)
+fi
+
 if [ -z $1 ]
 then
     echo "Usage sendCard cardFile opfab_url"
 else
     source ../getToken.sh "publisher_test" $url
-    export current_date_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000))
-    export current_date_in_milliseconds_from_epoch_plus_3minutes=$(($(date +%s%N)/1000000 + 3*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_1hours=$(($(date +%s%N)/1000000 + 60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_2hours=$(($(date +%s%N)/1000000 + 2*60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_4hours=$(($(date +%s%N)/1000000 + 4*60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_8hours=$(($(date +%s%N)/1000000 + 8*60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_12hours=$(($(date +%s%N)/1000000 + 12*60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_24hours=$(($(date +%s%N)/1000000 + 24*60*60*1000))
-    export current_date_in_milliseconds_from_epoch_plus_48hours=$(($(date +%s%N)/1000000 + 48*60*60*1000))
+    export current_date_in_milliseconds_from_epoch=$(($current_date_millis))
+    export current_date_in_milliseconds_from_epoch_plus_3minutes=$(($current_date_millis + 3*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_1hours=$(($current_date_millis + 60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_2hours=$(($current_date_millis + 2*60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_4hours=$(($current_date_millis + 4*60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_8hours=$(($current_date_millis + 8*60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_12hours=$(($current_date_millis + 12*60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_24hours=$(($current_date_millis + 24*60*60*1000))
+    export current_date_in_milliseconds_from_epoch_plus_48hours=$(($current_date_millis + 48*60*60*1000))
     echo "send card $1 (url: $url)"
     curl -X POST $url:2102/cards -H "Authorization: Bearer $token" -H "Content-type:application/json" --data "$(envsubst <$1)"
 fi
--- src/test/resources/perimeters/createAllPerimeter.sh
+++ src/test/resources/perimeters/createAllPerimeter.sh
@@ -13,26 +13,26 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 for d in *.json; do
-    perimeter=${d:0:-5}  #remove last 5 character 
+    perimeter=${d:0:$((${#d} - 5))} #remove last 5 character
     ./createPerimeter.sh  $perimeter $1
 done
 
- ./addPerimeterToGroup.sh  question Dispatcher $1
- ./addPerimeterToGroup.sh  question Planner $1
- ./addPerimeterToGroup.sh  defaultProcess Dispatcher $1
- ./addPerimeterToGroup.sh  defaultProcess Planner $1
- ./addPerimeterToGroup.sh  userCardExamples Dispatcher $1
- ./addPerimeterToGroup.sh  userCardExamples Planner $1
- ./addPerimeterToGroup.sh  userCardExamples Supervisor $1
- ./addPerimeterToGroup.sh  userCardExamples2 Dispatcher $1
- ./addPerimeterToGroup.sh  userCardExamples2 Planner $1
- ./addPerimeterToGroup.sh  userCardExamples2 Supervisor $1
- ./addPerimeterToGroup.sh  userCardExamples3 Dispatcher $1
- ./addPerimeterToGroup.sh  userCardExamples3 Planner $1
- ./addPerimeterToGroup.sh  userCardExamples3 Supervisor $1
- ./addPerimeterToGroup.sh  gridCooperation Dispatcher $1
- ./addPerimeterToGroup.sh  gridCooperation Planner $1
- ./addPerimeterToGroup.sh  gridCooperation Supervisor $1
- ./addPerimeterToGroup.sh  cypress Dispatcher $1
- ./addPerimeterToGroup.sh  cypress Planner $1
- ./addPerimeterToGroup.sh  questionReceiveOnly Supervisor $1
+ ./addPerimeterToGroup.sh question Dispatcher $1
+ ./addPerimeterToGroup.sh question Planner $1
+ ./addPerimeterToGroup.sh defaultProcess Dispatcher $1
+ ./addPerimeterToGroup.sh defaultProcess Planner $1
+ ./addPerimeterToGroup.sh userCardExamples Dispatcher $1
+ ./addPerimeterToGroup.sh userCardExamples Planner $1
+ ./addPerimeterToGroup.sh userCardExamples Supervisor $1
+ ./addPerimeterToGroup.sh userCardExamples2 Dispatcher $1
+ ./addPerimeterToGroup.sh userCardExamples2 Planner $1
+ ./addPerimeterToGroup.sh userCardExamples2 Supervisor $1
+ ./addPerimeterToGroup.sh userCardExamples3 Dispatcher $1
+ ./addPerimeterToGroup.sh userCardExamples3 Planner $1
+ ./addPerimeterToGroup.sh userCardExamples3 Supervisor $1
+ ./addPerimeterToGroup.sh gridCooperation Dispatcher $1
+ ./addPerimeterToGroup.sh gridCooperation Planner $1
+ ./addPerimeterToGroup.sh gridCooperation Supervisor $1
+ ./addPerimeterToGroup.sh cypress Dispatcher $1
+ ./addPerimeterToGroup.sh cypress Planner $1
+ ./addPerimeterToGroup.sh questionReceiveOnly Supervisor $1
