2fd101c88272f18c84519e7fbdafbe10b69abd4a
freddidierRTE <frederic-f.didier@rte-france.com>
2022-01-14 11:38:26 +0100

    usercard refacto : clean code
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/modules/usercard/datesForm/usercard-dates-form.component.ts
+++ ui/main/src/app/modules/usercard/datesForm/usercard-dates-form.component.ts
@@ -7,7 +7,7 @@
  * This file is part of the OperatorFabric project.
  */
 
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Subject} from 'rxjs';
 import {TimeService} from '@ofServices/time.service';
@@ -15,22 +15,19 @@ import {NgbDateStruct, NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
 import {DateTimeNgb, getDateTimeNgbFromMoment} from '@ofModel/datetime-ngb.model';
 import * as moment from 'moment-timezone';
 import {debounceTime, takeUntil} from 'rxjs/operators';
-import {TranslateService} from '@ngx-translate/core';
 import {DatesForm} from './dates-form.model';
-
 
 @Component({
     selector: 'of-usercard-dates-form',
     templateUrl: './usercard-dates-form.component.html',
 
 })
-export class UserCardDatesFormComponent implements OnInit, OnDestroy {
+export class UserCardDatesFormComponent implements OnInit, OnDestroy , OnChanges {
 
     @Input() public datesFormInputData: DatesForm;
 
     datesForm: FormGroup;
     unsubscribe$: Subject<void> = new Subject<void>();
-
 
     endDateMin: {year: number, month: number, day: number} = null;
     startDateMax: {year: number, month: number, day: number} = null;
@@ -38,8 +35,7 @@ export class UserCardDatesFormComponent implements OnInit, OnDestroy {
     dateTimeFilterChange = new Subject();
 
     constructor(
-        private timeService: TimeService,
-        protected translate: TranslateService
+        private timeService: TimeService
     ) {
     }
 
@@ -51,26 +47,43 @@ export class UserCardDatesFormComponent implements OnInit, OnDestroy {
             lttd: new FormControl(''),
 
         });
-
-
-
         this.setInitialDateValues();
-
         this.dateTimeFilterChange.pipe(
             takeUntil(this.unsubscribe$),
             debounceTime(1000),
         ).subscribe(() => this.setDateFilterBounds());
     }
 
-
-    private setInitialDateValues():void {
+    private setInitialDateValues(): void {
         if (this.datesForm) {
             if (this.datesFormInputData.startDate.isVisible) this.datesForm.get('startDate').setValue(this.datesFormInputData.startDate.initialEpochDate != null ? getDateTimeNgbFromMoment(moment(this.datesFormInputData.startDate.initialEpochDate)) : '');
             if (this.datesFormInputData.endDate.isVisible) this.datesForm.get('endDate').setValue(this.datesFormInputData.endDate.initialEpochDate != null ? getDateTimeNgbFromMoment(moment(this.datesFormInputData.endDate.initialEpochDate)) : '');
             if (this.datesFormInputData.lttd.isVisible) this.datesForm.get('lttd').setValue(this.datesFormInputData.lttd.initialEpochDate != null ? getDateTimeNgbFromMoment(moment(this.datesFormInputData.lttd.initialEpochDate)) : '');
         }
         this.setDateFilterBounds();
+    }
 
+    private setDateFilterBounds(): void {
+        if (this.datesForm) {
+            if (this.datesFormInputData.startDate.isVisible) {
+                this.endDateMin = {
+                    year: this.datesForm.value.startDate.date.year,
+                    month: this.datesForm.value.startDate.date.month,
+                    day: this.datesForm.value.startDate.date.day
+                };
+            } else {
+                this.endDateMin = null;
+            }
+            if (this.datesFormInputData.endDate.isVisible) {
+                this.startDateMax = {
+                    year: this.datesForm.value.endDate.date.year,
+                    month: this.datesForm.value.endDate.date.month,
+                    day: this.datesForm.value.endDate.date.day
+                };
+            } else {
+                this.startDateMax = null;
+            }
+        }
     }
 
     public getStartDateAsEpoch(): number {
@@ -85,44 +98,12 @@ export class UserCardDatesFormComponent implements OnInit, OnDestroy {
         return this.createTimestampFromValue(this.datesForm.get('lttd').value);
     }
 
-    onDateTimeChange(event: Event) {
+    onDateTimeChange() {
         this.dateTimeFilterChange.next(null);
     }
 
-    setDateFilterBounds(): void {
-        if (this.datesForm.value.startDate?.date) {
-            this.endDateMin = {
-                year: this.datesForm.value.startDate.date.year,
-                month: this.datesForm.value.startDate.date.month,
-                day: this.datesForm.value.startDate.date.day};
-        } else {
-            const today = moment();
-            this.endDateMin = {year: today.year(), month: today.month(), day: today.day()};
-        }
-        if (this.datesForm.value.endDate?.date) {
-            this.startDateMax = {
-                year: this.datesForm.value.endDate.date.year,
-                month: this.datesForm.value.endDate.date.month,
-                day: this.datesForm.value.endDate.date.day};
-        } else {
-            const tomorrow = moment().add(1, 'day');
-            this.startDateMax = {year: tomorrow.year(), month: tomorrow.month(), day: tomorrow.day()};
-        }
-    }
-
-
-
-    createTimestampFromValue = (value: any): number => {
-        const {date, time} = value;
-        if (date) {
-            return this.timeService.toNgBNumberTimestamp(this.transformToTimestamp(date, time));
-        } else {
-            return null;
-        }
-    };
-
-    transformToTimestamp(date: NgbDateStruct, time: NgbTimeStruct): string {
-        return new DateTimeNgb(date, time).formatDateTime();
+    ngOnChanges() {
+        this.setDateFilterBounds();
     }
 
     ngOnDestroy() {
@@ -130,6 +111,17 @@ export class UserCardDatesFormComponent implements OnInit, OnDestroy {
         this.unsubscribe$.complete();
     }
 
+    private createTimestampFromValue = (value: any): number => {
+        const {date, time} = value;
+        if (date) {
+            return this.timeService.toNgBNumberTimestamp(this.transformToTimestamp(date, time));
+        } else {
+            return null;
+        }
+    }
 
+    private transformToTimestamp(date: NgbDateStruct, time: NgbTimeStruct): string {
+        return new DateTimeNgb(date, time).formatDateTime();
+    }
 
 }
