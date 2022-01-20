450a530a06e446c76c1018e64e21cc4b39c20e5a
freddidierRTE <frederic-f.didier@rte-france.com>
2022-01-20 13:32:58 +0100

    Upgrade java to 11.0.14
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2018-2021, RTE (http://www.rte-france.com)
+# Copyright (c) 2018-2022, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,7 +13,7 @@ source ${BASH_SOURCE%/*}/load_variables.sh
 
 sdk install gradle 7.3
 sdk use gradle 7.3
-sdk install java 11.0.13-zulu
-sdk use java 11.0.13-zulu
+sdk install java 11.0.14-zulu
+sdk use java 11.0.14-zulu
 nvm install v14.15
 nvm use v14.15
