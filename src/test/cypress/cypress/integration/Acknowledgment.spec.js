e98daccfc8e854f3fb17ad8ae9cb46357b254d7e
freddidierRTE <frederic-f.didier@rte-france.com>
2022-02-07 13:34:23 +0100

    Improve acknowledgment cypress test for avoid falky tests
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/cypress/cypress/integration/Acknowledgment.spec.js
+++ src/test/cypress/cypress/integration/Acknowledgment.spec.js
@@ -48,6 +48,9 @@ describe('Acknowledgment  tests', function () {
 
         cy.loginOpFab('operator1_fr', 'test');
 
+        // Wait for all cards to be loaded (check the last one is present)
+        cy.get('#opfab-feed-light-card-cypress-message6');
+
         // Operator1 should see 6 cards in his feed
         cy.get('of-light-card').should('have.length', 6);
 
