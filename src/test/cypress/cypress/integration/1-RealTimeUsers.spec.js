df0faaf3644182cff6dbbf68cd90c97c17691814
Giovanni Ferrari <giovanni.ferrari@soft.it>
2022-02-10 18:49:42 +0100

    New users as members of ENTITY3_FR and ENTITY4_FR (#2456)
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- src/test/cypress/cypress/integration/1-RealTimeUsers.spec.js
+++ src/test/cypress/cypress/integration/1-RealTimeUsers.spec.js
@@ -39,14 +39,14 @@ describe ('RealTimeUsersPage',()=>{
         //click on "Real time users"
         cy.get('#opfab-navbar-right-menu-realtimeusers').click();
 
-        // we should have 18 disconnected entities/groups and 1 connected (operator3_fr for ENTITY1_FR/Dispatcher)
-        // we check the connected badge is for the first row/first column
+        // we should have 18 disconnected entities/groups and 1 connected (operator3_fr for ENTITY3_FR/Dispatcher)
+        // we check the connected badge is for the third row/first column
         cy.get('.badge').should('have.length', 19);
         cy.get('.bg-success').should('have.length', 1);
         cy.get('.bg-danger').should('have.length', 18);
-        cy.get('table').first().find('tr').eq(1).find('td').eq(0).find('.bg-success').should('have.length', 1);
-        cy.get('table').first().find('tr').eq(1).find('td').eq(0).find('span').eq(0).should('have.text', '1 ');
-        cy.get('table').first().find('tr').eq(1).find('td').eq(0).should('contain.text', '(operator3_fr)');
+        cy.get('table').first().find('tr').eq(3).find('td').eq(0).find('.bg-success').should('have.length', 1);
+        cy.get('table').first().find('tr').eq(3).find('td').eq(0).find('span').eq(0).should('have.text', '1 ');
+        cy.get('table').first().find('tr').eq(3).find('td').eq(0).should('contain.text', '(operator3_fr)');
 
         // we choose another screen (French Control Centers screen) and we check the titles
         cy.get('#of-realtimeusers-screen-selector').find('select').select('French Control Centers');
