0e5c28c6665aaac5feb00d40a9a60a6306754369
freddidierRTE <frederic-f.didier@rte-france.com>
2022-03-08 15:22:22 +0100

    Simplify jackson config (#2656)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/externalApp/src/main/java/org/opfab/externalapp/config/JacksonConfig.java
+++ src/test/externalApp/src/main/java/org/opfab/externalapp/config/JacksonConfig.java
@@ -10,37 +10,19 @@
 package org.opfab.externalapp.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.extern.slf4j.Slf4j;
 import org.opfab.springtools.json.InstantModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-/**
- * Json configuration
- *
- *
- */
 @Configuration
-@Slf4j
 public class JacksonConfig {
 
-  /**
-   * Builds object mapper adding java 8 custom configuration and business module configuration ({@link BusinessconfigModule})
-   * @param builder Spring internal {@link ObjectMapper} builder [injected]
-   * @return configured object mapper for json
-   */
   @Bean
   @Autowired
   public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
     ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-
-    // Some other custom configuration to support Java 8 features
-    objectMapper.registerModule(new Jdk8Module());
-    objectMapper.registerModule(new JavaTimeModule());
     objectMapper.registerModule(new InstantModule());
     return objectMapper;
   }
