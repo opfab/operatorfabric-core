e96ab0c9e1e5c487cbde00bd8ebaea7c89c6a62a
freddidierRTE <frederic-f.didier@rte-france.com>
2022-03-14 14:07:27 +0100

    Adujst cypress test for CI/CD
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/cypress/cypress/integration/ActivityArea.spec.js
+++ src/test/cypress/cypress/integration/ActivityArea.spec.js
@@ -87,6 +87,12 @@ describe ('ActivityAreaPage',()=>{
         // operator4_fr disconnect from ENTITY1_FR, ENTITY2_FR and ENTITY3_FR
         cy.get('#opfab-navbar-drop_user_menu').click();
         cy.get('#opfab-navbar-right-menu-activityarea').click();
+
+        // check every checkbox to let the time for the ui to set to true before we click 
+        cy.get('.opfab-checkbox').eq(0).find('input').should('be.checked');
+        cy.get('.opfab-checkbox').eq(1).find('input').should('be.checked');
+        cy.get('.opfab-checkbox').eq(2).find('input').should('be.checked');
+        cy.get('.opfab-checkbox').eq(3).find('input').should('be.checked');
         cy.get('.opfab-checkbox').contains('Control Center FR North').click();
         cy.get('.opfab-checkbox').contains('Control Center FR East').click();
         cy.get('.opfab-checkbox').contains('Control Center FR South').click();
