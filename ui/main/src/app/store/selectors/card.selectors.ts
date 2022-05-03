15233b909f0aa42f58a1fedd51d966c40e317a38
vlo-rte <valerie.longa@rte-france.com>
2022-05-03 16:10:30 +0200

    Feed screen : No spinner when it takes time to load a card (#2820)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- ui/main/src/app/store/selectors/card.selectors.ts
+++ ui/main/src/app/store/selectors/card.selectors.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2022, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,13 +11,20 @@
 
 import {AppState} from '@ofStore/index';
 import {createSelector} from '@ngrx/store';
-import {CardState} from '@ofStates/card.state';
+import {cardInitialState, CardState} from '@ofStates/card.state';
 import {Card} from '@ofModel/card.model';
 
 export const selectCardState = (state: AppState) => state.card;
 export const selectCardStateSelected =  createSelector(selectCardState, (cardState: CardState) => cardState.selected);
+
 export const selectCardStateSelectedWithChildCards =
-    createSelector(selectCardState, (cardState: CardState) => [cardState.selected, cardState.selectedChildCards]);
+    createSelector(selectCardState, (cardState: CardState) => {
+        if (cardState === cardInitialState)
+            return [cardInitialState];
+        else
+            return [cardState.selected, cardState.selectedChildCards];
+    });
+
 export const selectCardStateSelectedId =  createSelector(selectCardStateSelected, (card: Card) => {
     return card == null ? null : card.id;
 });
