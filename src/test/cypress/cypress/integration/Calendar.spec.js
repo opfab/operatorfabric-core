0bc1f5f89ada4a0e2a9438af581c204c09549cfb
vlo-rte <valerie.longa@rte-france.com>
2022-05-04 09:47:27 +0200

    Add cypress tests for calendar feature (#2917)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

---
+++ src/test/cypress/cypress/integration/Calendar.spec.js
@@ -0,0 +1,50 @@
+/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+describe ('Calendar screen tests',function () {
+
+    function openAndCheckCardDataQuality() {
+        cy.waitDefaultTime();
+        // click the card
+        cy.get('.fc-event-title').contains("Data quality").should("be.visible").click();
+
+        // detail card is present, check content and then close the card
+        cy.get('of-detail').should("be.visible");
+        cy.get('#opfab-card-title').should("have.text", "Data quality");
+        cy.get('#opfab-div-card-template').find('p').first().should("contain.text", "Info on quality degradation of the main server");
+        cy.get('#opfab-close-card').click();
+    }
+
+    before('Set up configuration', function () {
+        cy.loadTestConf();
+        cy.deleteAllCards();
+    });
+
+
+    it('Check calendar screen', function () {
+        cy.send6TestCards();
+        cy.loginOpFab('operator1_fr','test');
+
+        cy.get('#opfab-navbarContent').find('#opfab-calendar-menu').click();
+
+        // test month view (view by default)
+        cy.get('.opfab-calendar-event').should("have.length", 1); // only one card should be present
+        openAndCheckCardDataQuality();
+
+        // test week view
+        cy.get('button').contains(/^week$/).should("be.visible").click();
+        cy.get('.opfab-calendar-event').should("have.length", 1); // only one card should be present
+        openAndCheckCardDataQuality();
+
+        // test day view
+        cy.get('button').contains(/^day$/).should("be.visible").click();
+        cy.get('.opfab-calendar-event').should("have.length", 1); // only one card should be present
+        openAndCheckCardDataQuality();
+    })
+})
