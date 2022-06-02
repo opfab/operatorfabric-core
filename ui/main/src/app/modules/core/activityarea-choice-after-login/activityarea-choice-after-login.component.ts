bc1df21fff8b8b553821f51eaa5750f26f60ad15
freddidierRTE <frederic-f.didier@rte-france.com>
2022-06-02 17:06:56 +0200

    App component refacto - extract activity area choice component
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ ui/main/src/app/modules/core/activityarea-choice-after-login/activityarea-choice-after-login.component.ts
@@ -0,0 +1,62 @@
+/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+import {Component, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {ConfigService} from '@ofServices/config.service';
+import {EntitiesService} from '@ofServices/entities.service';
+import {UserService} from '@ofServices/user.service';
+import {Subject} from 'rxjs';
+
+@Component({
+    selector: 'of-activityarea-choice-after-login',
+    templateUrl: './activityarea-choice-after-login.component.html'
+})
+export class ActivityAreaChoiceAferLoginComponent implements OnInit {
+    private modalRef: NgbModalRef;
+    @ViewChild('activityArea') activityAreaPopupRef: TemplateRef<any>;
+
+    @Output() public activityAreaChoiceDone = new Subject();
+
+    constructor(
+        private userService: UserService,
+        private entitiesService: EntitiesService,
+        private modalService: NgbModal,
+        private configService: ConfigService
+    ) {}
+
+    ngOnInit(): void {
+        if (this.configService.getConfigValue('selectActivityAreaOnLogin', false)) this.confirmActivityArea();
+        else this.sendActivityAreaChoiceDone();
+    }
+
+    private confirmActivityArea() {
+        const login = this.userService.getCurrentUserWithPerimeters().userData.login;
+        this.userService.getUser(login).subscribe((currentUser) => {
+            const entities = this.entitiesService.getEntitiesFromIds(currentUser.entities);
+            if (entities.filter((entity) => entity.entityAllowedToSendCard).length > 1)
+                this.modalRef = this.modalService.open(this.activityAreaPopupRef, {
+                    centered: true,
+                    backdrop: 'static',
+                    size: 'xl'
+                });
+            else this.sendActivityAreaChoiceDone();
+        });
+    }
+
+    onActivityAreaConfirm() {
+        this.modalRef.close();
+        this.sendActivityAreaChoiceDone();
+    }
+
+    sendActivityAreaChoiceDone() {
+        this.activityAreaChoiceDone.next(true);
+        this.activityAreaChoiceDone.complete();
+    }
+}
