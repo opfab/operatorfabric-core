d3d9eb2a7427ac80772c6a4d22d4164149f41ae9
freddidierRTE <frederic-f.didier@rte-france.com>
2022-06-02 21:58:47 +0200

    App component refacto - extract account already used component
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ ui/main/src/app/modules/core/account-already-used/account-already-used.component.ts
@@ -0,0 +1,71 @@
+/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+import {Component, Input, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {Store} from '@ngrx/store';
+import {LogOption, OpfabLoggerService} from '@ofServices/logs/opfab-logger.service';
+import {UserService} from '@ofServices/user.service';
+import {TryToLogOutAction} from '@ofStore/actions/authentication.actions';
+import {Subject} from 'rxjs';
+
+@Component({
+    selector: 'of-account-already-used',
+    templateUrl: './account-already-used.component.html'
+})
+export class AccountAlreadyUsedComponent implements OnInit {
+    @Input() public userLogin: string;
+    @Output() public accountAlreadyUsedCheckDone = new Subject();
+
+    @ViewChild('sessionAlreadyInUse') sessionAlreadyInUsePopupRef: TemplateRef<any>;
+
+    private questionModal: NgbModalRef;
+
+    constructor(
+        private store: Store,
+        private userService: UserService,
+        private modalService: NgbModal,
+        private logger: OpfabLoggerService
+    ) {}
+
+    ngOnInit(): void {
+        this.checkIfUserIsAlreadyConnected();
+    }
+
+    private checkIfUserIsAlreadyConnected() {
+        this.userService.willNewSubscriptionDisconnectAnExistingSubscription().subscribe((isUserAlreadyConnected) => {
+            this.logger.info('Login ' + this.userLogin + ' is already connected', LogOption.REMOTE);
+            if (isUserAlreadyConnected) {
+                this.questionModal = this.modalService.open(this.sessionAlreadyInUsePopupRef, {
+                    centered: true,
+                    backdrop: 'static'
+                });
+            } else {
+                this.sendEventAccountAlreadyInUseCheckDone();
+            }
+        });
+    }
+
+    public loginEvenIfAccountIsAlreadyUsed(): void {
+        this.logger.info('Login as ' + this.userLogin + ' even if account is already used ', LogOption.REMOTE);
+        this.questionModal.close();
+        this.sendEventAccountAlreadyInUseCheckDone();
+    }
+
+    public logoutBecauseAccountIsAlreadyUsed() {
+        this.logger.info('Logout with user ' + this.userLogin + ' because account already used ', LogOption.REMOTE);
+        this.questionModal.close();
+        this.store.dispatch(new TryToLogOutAction());
+    }
+
+    private sendEventAccountAlreadyInUseCheckDone() {
+        this.accountAlreadyUsedCheckDone.next(true);
+        this.accountAlreadyUsedCheckDone.complete();
+    }
+}
