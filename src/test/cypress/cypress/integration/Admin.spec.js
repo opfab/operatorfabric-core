e466a3e5433510efafbfe2248938d7ea6a677f4c
Giovanni Ferrari <giovanni.ferrari@soft.it>
2022-06-06 12:40:25 +0200

    Add cypress tests for entity administration (#2935)
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- src/test/cypress/cypress/integration/Admin.spec.js
+++ src/test/cypress/cypress/integration/Admin.spec.js
@@ -28,7 +28,7 @@ describe ('AdminPage',()=>{
         cy.get('.opfab-pagination').should('contain.text', ' Results number  : 12');
 
         // Add new user
-        cy.get('#addItem').click();
+        cy.get('#add-item').click();
 
         cy.get('of-edit-user-modal').should('exist');
 
@@ -97,8 +97,6 @@ describe ('AdminPage',()=>{
 
         cy.get('#opfab-admin-user-btn-save').click();
 
-        cy.waitDefaultTime();
-
         // Check user is updated
         cy.get('.opfab-pagination').should('contain.text', ' Results number  : 13');
 
@@ -127,4 +125,128 @@ describe ('AdminPage',()=>{
         cy.countAgGridTableRows('ag-grid-angular', 2);
     });
  
+    it('List, add, edit, delete entites', ()=> {
+
+        cy.loginOpFab('admin', 'test');
+
+        //Click on user menu (top right of the screen)
+        cy.get('#opfab-navbar-drop-user-menu').click();
+
+         //Click on "Administration"
+        cy.get('#opfab-navbar-right-menu-admin').click();
+
+        //Click on tab "Entities"
+        cy.get('#opfab-tabs').find('li').eq(1).click();
+
+        // Check first page has 10 rows
+        cy.countAgGridTableRows('ag-grid-angular', 10);
+
+        // Pagination should display ' Results number  : 13 '
+        cy.get('.opfab-pagination').should('contain.text', ' Results number  : 13');
+
+        // Add new entity
+        cy.get('#add-item').click();
+
+        cy.get('of-edit-entity-modal').should('exist');
+
+        // Check id and name are mandatory
+        cy.get('#opfab-admin-edit-btn-add').should('be.disabled');
+
+        cy.get('#opfab-id').type('entityId');
+
+        cy.get('#opfab-admin-edit-btn-add').should('be.disabled');
+
+        cy.get('#opfab-name').type('entity name');
+
+        cy.get('#opfab-admin-edit-btn-add').should('not.be.disabled');
+
+        cy.get('#opfab-description').type('entity description');
+
+        cy.get('#opfab-entity-allowed-to-send-card').check({force: true});
+
+        cy.get('#opfab-parents').click();
+        cy.get('#opfab-parents').find('.vscomp-option-text').eq(1).click({ force: true });
+        cy.get('#opfab-parents').click();
+
+
+        cy.get('tag-input').find('[aria-label="Add label"]').eq(0).type('Label1');
+
+        cy.get('#opfab-admin-edit-btn-add').click();
+
+        cy.get('.opfab-pagination').should('contain.text', ' Results number  : 14');
+
+        cy.get('ngb-pagination').find('.page-link').eq(2).click();
+
+        cy.countAgGridTableRows('ag-grid-angular', 4);
+
+        cy.agGridCellShould('ag-grid-angular', 3, 0, 'have.text', 'entityId');
+        cy.agGridCellShould('ag-grid-angular', 3, 1, 'have.text', 'entity name');
+        cy.agGridCellShould('ag-grid-angular', 3, 2, 'have.text', 'entity description');
+        cy.agGridCellShould('ag-grid-angular', 3, 3, 'have.text', 'YES');
+        cy.agGridCellShould('ag-grid-angular', 3, 4, 'have.text', 'Control Center FR North');
+
+        // Edit previously created entity
+        cy.clickAgGridCell('ag-grid-angular', 3, 5, 'of-action-cell-renderer');
+
+        cy.get('of-edit-entity-modal').should('exist');
+
+        cy.get('#opfab-id').should('not.exist');
+
+        cy.get('.modal-title').should('contain.text', 'entityId');
+
+
+
+        cy.get('#opfab-name').type(' updated');
+
+        cy.get('#opfab-description').type(' updated');
+
+        cy.get('#opfab-entity-allowed-to-send-card').should('be.checked');
+        cy.get('#opfab-entity-allowed-to-send-card').uncheck({force: true});
+
+        cy.get('#opfab-parents').click();
+        // Deselect old parents
+        cy.get('#opfab-parents').find('.vscomp-option-text').eq(1).click({ force: true });
+        // Select new parent
+        cy.get('#opfab-parents').find('.vscomp-option-text').eq(2).click({ force: true });
+        cy.get('#opfab-parents').click();
+
+        cy.get('tag-input').find('[aria-label="Label1"]').should('exist');
+        // Add Label2
+        cy.get('tag-input').find('[aria-label="Add label"]').eq(0).type('Label2');
+
+        // Remove Label1
+         cy.get('tag-input').find('[aria-label="Remove tag"]').eq(0).click();
+         cy.get('tag-input').find('[aria-label="Label1"]').should('not.exist');
+         cy.get('tag-input').find('[aria-label="Label2"]').should('exist');
+
+        cy.get('#opfab-admin-entity-btn-save').click();
+
+        // Check entity is updated
+        cy.get('.opfab-pagination').should('contain.text', ' Results number  : 14');
+
+        cy.get('ngb-pagination').find('.page-link').eq(2).click();
+
+        cy.countAgGridTableRows('ag-grid-angular', 4);
+
+        cy.agGridCellShould('ag-grid-angular', 3, 0, 'have.text', 'entityId');
+        cy.agGridCellShould('ag-grid-angular', 3, 1, 'have.text', 'entity name updated');
+        cy.agGridCellShould('ag-grid-angular', 3, 2, 'have.text', 'entity description updated');
+        cy.agGridCellShould('ag-grid-angular', 3, 3, 'have.text', 'NO');
+        cy.agGridCellShould('ag-grid-angular', 3, 4, 'have.text', 'Control Center FR South');
+
+        // Delete previously created entity
+        cy.clickAgGridCell('ag-grid-angular', 3, 6, 'of-action-cell-renderer');
+
+        cy.get('of-confirmation-dialog').should('exist');
+
+        cy.get('#opfab-admin-confirmation-btn-ok').click();
+
+        cy.waitDefaultTime();
+
+        //Check entity was deleted
+        cy.get('.opfab-pagination').should('contain.text', ' Results number  : 13');
+
+        cy.countAgGridTableRows('ag-grid-angular', 3);
+
+    });
 })
