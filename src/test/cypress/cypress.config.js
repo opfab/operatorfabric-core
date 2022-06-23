b67ec2ac42013eac891a802e0ccac28b6b053fdb
vlo-rte <valerie.longa@rte-france.com>
2022-06-23 12:19:36 +0200

    Migrate to cypress 10 (#3148)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

---
+++ src/test/cypress/cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const readXlsx = require("./cypress/plugins/read-xlsx");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:2002/ui/",
+    viewportWidth: 1848,
+    viewportHeight: 949,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 6000,
+    specPattern: 'cypress/integration/**/*.spec.js',
+
+    env: {
+      defaultWaitTime: 500,
+      host: "http://localhost",
+    },
+
+    setupNodeEvents(on, config) {
+      on('task', {
+        'readXlsx': readXlsx.read,
+        'list': readXlsx.list,
+        'deleteFile': readXlsx.deleteFile})
+    }
+  }
+});
