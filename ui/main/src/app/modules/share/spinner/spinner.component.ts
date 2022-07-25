8a9488b2dc0c363782dfb9a0d13733a8b6904a54
vlo-rte <valerie.longa@rte-france.com>
2022-07-25 11:42:22 +0200

    Add spinner when click on "acknowledge and close" (#3351)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- ui/main/src/app/modules/share/spinner/spinner.component.ts
+++ ui/main/src/app/modules/share/spinner/spinner.component.ts
@@ -7,25 +7,52 @@
  * This file is part of the OperatorFabric project.
  */
 
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {NgbModal, NgbModalOptions, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'of-spinner',
     templateUrl: './spinner.component.html',
     styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, OnDestroy {
     @Input() loadingText = "shared.loadingInProgress";
     @Input() timeBeforeDisplayingSpinner = 500;
+    @Input() seeInModal = false;
+
+    @ViewChild('spinnerInModal') spinnerInModalPopupRef: TemplateRef<any>;
+    private modalRef: NgbModalRef;
+    private destroy = false;
 
     mustDisplaySpinner = false;
+
+    constructor(private modalService: NgbModal) {}
 
     ngOnInit() {
         setTimeout(() => {
             this.mustDisplaySpinner = true;
+
+            if (this.seeInModal) {
+                if (! this.destroy) {
+                    this.modalRef = this.openInModal();
+                }
+            }
         }, this.timeBeforeDisplayingSpinner);
     }
 
+    private openInModal(): NgbModalRef {
+        const modalOptions: NgbModalOptions = {
+            centered: true,
+            backdrop: 'static', // Modal shouldn't close even if we click outside it
+            size: 'sm'
+        };
+        return this.modalService.open(this.spinnerInModalPopupRef, modalOptions);
+    }
 
-
+    ngOnDestroy() {
+        this.destroy = true;
+        if (!!this.modalRef) {
+            this.modalRef.close();
+        }
+    }
 }
