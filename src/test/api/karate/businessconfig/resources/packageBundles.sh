cec4a93165c6b4b309b0ce7bc84b33944df64fcc
vlo-rte <valerie.longa@rte-france.com>
2022-08-19 00:29:25 +0200

    Refuse cards if no translation file or no translation (#3178)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- src/test/api/karate/businessconfig/resources/packageBundles.sh
+++ src/test/api/karate/businessconfig/resources/packageBundles.sh
@@ -3,7 +3,7 @@ tar -czvf bundle_api_test.tar.gz config.json i18n.json css/ template/
 mv bundle_api_test.tar.gz ../
 cd ..
 
-cd bundle_api_test_v2 
+cd bundle_api_test_v2
 tar -czvf bundle_api_test_v2.tar.gz config.json i18n.json css/ template/ 
 mv bundle_api_test_v2.tar.gz ../
 cd ..
@@ -13,7 +13,7 @@ tar -czvf bundle_test_action.tar.gz config.json i18n.json template/
 mv bundle_test_action.tar.gz ../
 cd ..
 
-cd bundle_api_test_apogee 
+cd bundle_api_test_apogee
 tar -czvf bundle_api_test_apogee.tar.gz config.json i18n.json css/ template/ 
 mv bundle_api_test_apogee.tar.gz ../
 cd ..
@@ -26,6 +26,11 @@ cd ../../../api/karate/businessconfig/resources
 cd bundle_api_test_with_newState
 tar -czvf bundle_api_test_with_newState.tar.gz config.json i18n.json css/ template/
 mv bundle_api_test_with_newState.tar.gz ../
+cd ..
+
+cd bundle_api_test_without_i18n_file
+tar -czvf bundle_api_test_without_i18n_file.tar.gz config.json css/ template/
+mv bundle_api_test_without_i18n_file.tar.gz ../
 cd ..
 
 cd bundle_process_1
