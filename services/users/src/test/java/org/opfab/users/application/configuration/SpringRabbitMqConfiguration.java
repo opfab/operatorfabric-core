5c6dd3bf5265f164fe5be681522c05b4dbac803f
Giovanni Ferrari <giovanni.ferrari@soft.it>
2022-08-30 18:18:46 +0200

    Configure rabbitmq connection retry policy on test environment
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

---
+++ services/users/src/test/java/org/opfab/users/application/configuration/SpringRabbitMqConfiguration.java
@@ -0,0 +1,46 @@
+/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+package org.opfab.users.application.configuration;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+/*
+ * Configuration of a retry policy for rabbitMq to avoid errors during unit tests 
+ * ExponentialBackOffPolicy increases the back off period for each retry attempt using the exponential function
+ */
+@Configuration
+public class SpringRabbitMqConfiguration {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        RetryTemplate retryTemplate = new RetryTemplate();
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(500);
+        backOffPolicy.setMultiplier(10.0);
+        backOffPolicy.setMaxInterval(10000);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        template.setRetryTemplate(retryTemplate);
+        return template;
+    }
+}
