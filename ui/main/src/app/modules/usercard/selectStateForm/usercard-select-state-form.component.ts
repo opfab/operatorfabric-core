6db2a5bf64f79277332e2c757b485f7848ef07f8
freddidierRTE <frederic-f.didier@rte-france.com>
2022-09-29 11:51:02 +0200

    Correct bug : In usercard , sometimes template does not change when changing process (#3651)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/modules/usercard/selectStateForm/usercard-select-state-form.component.ts
+++ ui/main/src/app/modules/usercard/selectStateForm/usercard-select-state-form.component.ts
@@ -232,8 +232,17 @@ export class UserCardSelectStateFormComponent implements OnInit, OnDestroy {
             if (!!process) {
                 this.stateOptions = this.statesPerProcesses.get(process);
                 if (!this.cardIdToEdit) {
-                    this.selectedProcess = process;
+                    const oldSelectedState = this.selectedState;
                     this.selectedState = this.stateOptions[0].value;
+
+                   // in case the state is the same as before , the selected value does not change for the select component 
+                   // so we need to send the event here instead of waiting for the event state change 
+                    if (this.selectedState === oldSelectedState) {
+                        this.stateChange.emit({
+                            selectedProcessId: this.selectStateForm.get('usercardProcess').value,
+                            state: this.selectStateForm.get('usercardState').value
+                        });
+                    }
                 }
             }
         });
