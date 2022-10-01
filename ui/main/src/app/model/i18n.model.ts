aadd6fd8aa0869b11cc2f5d7c55fa2a081ab9410
freddidierRTE <frederic-f.didier@rte-france.com>
2022-10-01 22:09:36 +0200

    Remove confusing custom Map type
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/model/i18n.model.ts
+++ ui/main/src/app/model/i18n.model.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2020, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2022, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,8 +7,7 @@
  * This file is part of the OperatorFabric project.
  */
 
-import {Map} from '@ofModel/map';
 
 export class I18n {
-    constructor(readonly key: string, readonly parameters?: Map<string>) {}
+    constructor(readonly key: string, readonly parameters?: Map<string,string>) {}
 }
