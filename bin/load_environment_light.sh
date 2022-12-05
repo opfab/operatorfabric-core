220a485cacd730f131c0cca2b98eba3d25e3eddf
freddidierRTE <frederic-f.didier@rte-france.com>
2022-12-05 18:01:18 +0100

    Migrate to java 17 (#2105)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -11,7 +11,7 @@
 
 source ${BASH_SOURCE%/*}/load_variables.sh
 
-sdk install java 11.0.15-zulu
-sdk use java 11.0.15-zulu
+sdk install java 17.0.4.1-zulu
+sdk use java 17.0.4.1-zulu
 nvm install v18.12.1
 nvm use v18.12.1
