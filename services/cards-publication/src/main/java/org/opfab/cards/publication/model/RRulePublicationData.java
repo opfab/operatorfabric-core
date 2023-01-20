f5d9c0c75a79876d7ff72946eed5cd5bb88154e2
vlo-rte <valerie.longa@rte-france.com>
2023-01-20 17:36:34 +0100

    New karate tests for RRule object and add object in the back (#3773)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- services/cards-publication/src/main/java/org/opfab/cards/publication/model/RRulePublicationData.java
+++ services/cards-publication/src/main/java/org/opfab/cards/publication/model/RRulePublicationData.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+/* Copyright (c) 2022-2023, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -41,4 +41,6 @@ public class RRulePublicationData implements RRule {
     private List<Integer> bymonth;
     private List<Integer> byhour;
     private List<Integer> byminute;
+    @Builder.Default
+    private String tzid = "Europe/Paris";
 }
