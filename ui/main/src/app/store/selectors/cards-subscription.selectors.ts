fd4623d1c445a5892f48021a3b56f641bca59c99
freddidierRTE <frederic-f.didier@rte-france.com>
2023-01-29 17:23:02 +0100

    Refacto UI : remove ngrx for reload event processing
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/store/selectors/cards-subscription.selectors.ts
+++ ui/main/src/app/store/selectors/cards-subscription.selectors.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2022, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2023, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,5 +13,3 @@ import {AppState} from '@ofStore/index';
 export const selectCardsSubscriptionState = (state: AppState) => state.cardsSubscription;
 
 export const selectSubscriptionOpen = createSelector(selectCardsSubscriptionState, (state) => state.subscriptionOpen);
-
-export const selectRelodRequested = createSelector(selectCardsSubscriptionState, (state) => state.reloadRequested);
