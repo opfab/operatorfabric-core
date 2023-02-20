110048a9ba2c980d70ba4233cc253f2edffd9101
freddidierRTE <frederic-f.didier@rte-france.com>
2023-02-20 07:51:06 +0100

    Refacto UI : refacto about component to be compliant with new UI software architecture
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/app/modules/navbar/navbar.module.ts
+++ ui/main/src/app/modules/navbar/navbar.module.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021, RTE (http://www.rte-france.com)
+/* Copyright (c) 2021-2023, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -18,7 +18,7 @@ import {MenuLinkComponent} from './menus/menu-link/menu-link.component';
 import {AppRoutingModule} from 'app/router/app-routing.module';
 import {UserCardModule} from '../usercard/usercard.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AboutComponent} from '../about/about.component';
+import {AboutComponent} from '../core/about/about.component';
 import {SpinnerModule} from "../share/spinner/spinner.module";
 
 @NgModule({
