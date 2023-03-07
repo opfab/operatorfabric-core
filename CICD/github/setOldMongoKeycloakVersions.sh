08521569e5a8cb4cbc24e111fbfd478975d40807
freddidierRTE <frederic-f.didier@rte-france.com>
2023-03-07 14:57:46 +0100

    Correct CI test with old keycloak version (#4290)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- CICD/github/setOldMongoKeycloakVersions.sh
+++ CICD/github/setOldMongoKeycloakVersions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2022, RTE (http://www.rte-france.com)
+# Copyright (c) 2022-2023, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,7 +9,7 @@
 # This file is part of the OperatorFabric project.
  
 echo "Set Keycloack version to 12.0.4"
-sed -i "s/jboss\/keycloak:.*$/jboss\/keycloak:12.0.4/g" config/docker/docker-compose.yml
+sed -i "s/quay\.io\/keycloak.*$/jboss\/keycloak:12.0.4/g" config/docker/docker-compose.yml
+cp config/keycloak/export/dev-realm-keycloak12.json config/keycloak/export/dev-realm.json
 echo "Set mongo version to 4.0-bionic"
 sed -i "s/mongo:.*$/mongo:4.0-bionic/g" config/docker/docker-compose.yml
-
