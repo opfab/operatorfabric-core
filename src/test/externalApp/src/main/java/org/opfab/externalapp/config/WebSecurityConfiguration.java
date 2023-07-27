c87a04b0062cae90ba2273b4b65cf7a59cd96e88
Duchatelle Hubert <hubert.duchatelle@rte-france.com>
2023-07-27 11:35:57 +0200

    Remove use of deprecated methods of HttpSecurity (#4765)
    Signed-off-by: Duchatelle Hubert <hubert.duchatelle@rte-france.com>

--- src/test/externalApp/src/main/java/org/opfab/externalapp/config/WebSecurityConfiguration.java
+++ src/test/externalApp/src/main/java/org/opfab/externalapp/config/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -52,5 +52,4 @@ public class WebSecurityConfiguration {
                     );
         }
     }
-
 }
