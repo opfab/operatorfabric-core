64f9f2f939c8e2f372a2f1d703b7882c82377906
Duchatelle Hubert <hubert.duchatelle@rte-france.com>
2023-07-27 11:32:01 +0200

    Remove use of deprecated methods of HttpSecurity (#4765)
    Signed-off-by: Duchatelle Hubert <hubert.duchatelle@rte-france.com>

--- src/test/externalApp/src/main/java/org/opfab/externalapp/config/WebSecurityConfiguration.java
+++ src/test/externalApp/src/main/java/org/opfab/externalapp/config/WebSecurityConfiguration.java
@@ -13,6 +13,7 @@ package org.opfab.externalapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,20 +35,21 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         configureCommon(http, authenticationRequired);
         http
-                .oauth2ResourceServer()
-                .jwt();
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                    .jwt(Customizer.withDefaults()));
 
         return http.build();
     }
 
     public static void configureCommon(final HttpSecurity http, boolean authenticationRequired) throws Exception {
-        http.csrf().disable();
+        http.csrf(csrf -> csrf.disable());
         if (authenticationRequired) {
             http
-                    .authorizeHttpRequests()
-                    .requestMatchers("/test").authenticated()
-                    .requestMatchers("/test/**").authenticated()
-                    .requestMatchers("/**").permitAll();
+                    .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/test").authenticated()
+                        .requestMatchers("/test/**").authenticated()
+                        .requestMatchers("/**").permitAll()
+                    );
         }
     }
 
