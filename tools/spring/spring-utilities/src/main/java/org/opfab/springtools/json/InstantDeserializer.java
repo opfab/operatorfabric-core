45d17344ca03c960ac01be63091e38dc5d837089
freddidierRTE <frederic-f.didier@rte-france.com>
2023-09-11 17:04:36 +0200

    Solve potential null exception (Ref audit : CID:16669)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- tools/spring/spring-utilities/src/main/java/org/opfab/springtools/json/InstantDeserializer.java
+++ tools/spring/spring-utilities/src/main/java/org/opfab/springtools/json/InstantDeserializer.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2022, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2023, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -42,6 +42,7 @@ public class InstantDeserializer extends StdDeserializer<Instant> {
         switch (parser.currentTokenId()) {
             case JsonTokenId.ID_START_OBJECT:
                 final ObjectNode node = new ObjectMapper().readValue(parser, ObjectNode.class);
+                if (node==null) throw new IOException("Impossible to deserialize Instant object");
                 return Instant.ofEpochSecond(Long.parseLong(node.get("epochSecond").toString()),Long.parseLong(node.get("nano").toString()));
 
             case JsonTokenId.ID_NUMBER_INT:
