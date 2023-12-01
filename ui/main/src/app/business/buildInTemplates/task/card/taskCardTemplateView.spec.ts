ed2749218c6d091b710f0efa54882abf532aae1f
vlo-rte <valerie.longa@rte-france.com>
2023-12-01 16:10:54 +0100

    Task advanced : a last comma that should not be displayed (#5496)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- ui/main/src/app/business/buildInTemplates/task/card/taskCardTemplateView.spec.ts
+++ ui/main/src/app/business/buildInTemplates/task/card/taskCardTemplateView.spec.ts
@@ -48,7 +48,7 @@ describe('Question UserCard template', () => {
             };
         };
         let text = view.fillTexts();
-        expect(text.textForBysetpos).toEqual("Translation of buildInTemplate.taskCard.the Translation of buildInTemplate.taskCard.first, ");
+        expect(text.textForBysetpos).toEqual("Translation of buildInTemplate.taskCard.the Translation of buildInTemplate.taskCard.first ");
         expect(text.textForByWeekday).toEqual(" Translation of shared.calendar.monday");
         expect(text.textForBymonthday).toEqual("Translation of buildInTemplate.taskCard.firstDayOfTheMonth<br/><br/>");
         expect(text.textForBymonth).toEqual("Translation of buildInTemplate.taskCard.in Translation of shared.calendar.january");
