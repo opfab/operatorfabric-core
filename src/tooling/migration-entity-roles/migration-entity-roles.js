3ae3f79515d141aa97a2cd3fddd76bb54187cf2e
ClementBouvierN <clement.bouvierneveu@rte-france.com>
2024-01-10 09:21:46 +0100

    Added a migration script for entity roles #5615
    Signed-off-by: ClementBouvierN <clement.bouvierneveu@rte-france.com>

---
+++ src/tooling/migration-entity-roles/migration-entity-roles.js
@@ -0,0 +1,50 @@
+/* Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+const mongoClient = require('mongodb').MongoClient;
+
+const login = process.argv[4];
+const pwd = process.argv[5];
+
+const databaseUrl = 'mongodb://' + login + ':' + pwd + '@' + process.argv[2] + ':' + process.argv[3] + '/';
+const databaseName = 'operator-fabric';
+
+async function updateEntityRoles(databaseUrl, databaseName) {
+
+  console.log("Migration of entity roles is starting");
+
+  const client = new mongoClient(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+
+    console.log("Connection to the mongo database has been established");
+
+    const db = client.db(databaseName);
+    const entitiesCollection = db.collection('entity');
+
+    await entitiesCollection.updateMany(
+      { entityAllowedToSendCard: true },
+      { $set: { roles: ["ACTIVITY_AREA", "CARD_SENDER", "CARD_RECEIVER"] } }
+    );
+
+    await entitiesCollection.updateMany(
+      { entityAllowedToSendCard: false },
+      { $set: { roles: ["CARD_RECEIVER"] } }
+    );
+
+    console.log("Entity roles have been successfully updated");
+
+  } finally {
+    await client.close();
+    console.log("Connection to the mongo database has been terminated");
+  }
+}
+
+updateEntityRoles(databaseUrl, databaseName);
