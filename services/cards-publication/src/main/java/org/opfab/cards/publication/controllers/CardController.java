73243f66ba1f07fb31056ac3b8f73e172b10622b
freddidierRTE <frederic-f.didier@rte-france.com>
2024-01-25 08:57:13 +0100

    Solve issue : JWT Login clain parameter is not take into account for read and unack (#5799)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- services/cards-publication/src/main/java/org/opfab/cards/publication/controllers/CardController.java
+++ services/cards-publication/src/main/java/org/opfab/cards/publication/controllers/CardController.java
@@ -178,7 +178,7 @@ public class CardController {
         OpFabJwtAuthenticationToken jwtPrincipal = (OpFabJwtAuthenticationToken) principal;
         CurrentUserWithPerimeters user = (CurrentUserWithPerimeters) jwtPrincipal.getPrincipal();
 
-        UserBasedOperationResult result = cardProcessingService.processUserRead(cardUid, principal.getName());
+        UserBasedOperationResult result = cardProcessingService.processUserRead(cardUid, user.getUserData().getLogin());
         if (!result.isCardFound())
             response.setStatus(404);
         else {
@@ -203,7 +203,7 @@ public class CardController {
             HttpServletResponse response) {
         OpFabJwtAuthenticationToken jwtPrincipal = (OpFabJwtAuthenticationToken) principal;
         CurrentUserWithPerimeters user = (CurrentUserWithPerimeters) jwtPrincipal.getPrincipal();
-        UserBasedOperationResult result = cardProcessingService.deleteUserAcknowledgement(cardUid, principal.getName());
+        UserBasedOperationResult result = cardProcessingService.deleteUserAcknowledgement(cardUid, user.getUserData().getLogin());  
         if (!result.isCardFound())
             response.setStatus(404);
         else {
@@ -228,7 +228,7 @@ public class CardController {
             HttpServletResponse response) {
         OpFabJwtAuthenticationToken jwtPrincipal = (OpFabJwtAuthenticationToken) principal;
         CurrentUserWithPerimeters user = (CurrentUserWithPerimeters) jwtPrincipal.getPrincipal();
-        UserBasedOperationResult result = cardProcessingService.deleteUserRead(cardUid, principal.getName());
+        UserBasedOperationResult result = cardProcessingService.deleteUserRead(cardUid, user.getUserData().getLogin());
         if (!result.isCardFound())
             response.setStatus(404);
         else {
