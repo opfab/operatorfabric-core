9dc3888808e5b6da76cd45f9430dc122c2ff5520
freddidierRTE <frederic-f.didier@rte-france.com>
2024-02-06 17:06:30 +0100

    Remove unneeded local mongo configuration (following previous refactorings)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- tools/spring/spring-mongo-utilities/src/main/java/org/opfab/springtools/configuration/mongo/MongoConfiguration.java
+++ tools/spring/spring-mongo-utilities/src/main/java/org/opfab/springtools/configuration/mongo/MongoConfiguration.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2023, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2024, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -17,28 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-
-/**
- * Mongo configuration.
- *  extends AbstractReactiveMongoConfiguration and overrides for customization
- * <ul>
- * <li>Standard cluster client configuration</li>
- * <li>Reactive cluster client configuration</li>
- * <li>Builds converter form {@link AbstractLocalMongoConfiguration#converterList()}</li>
- * <li>Mongo bean validation</li>
- * </ul>
- *
- *
- */
 
 @Configuration
 public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
@@ -75,21 +57,7 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
         return new ValidatingMongoEventListener(localValidatorFactoryBean);
     }
 
-    @Bean
-    public MongoCustomConversions customConversions(@Autowired AbstractLocalMongoConfiguration localConfiguration) {
-        return new MongoCustomConversions(localConfiguration.converterList());
-    }
 
-    @Override
-    public MappingMongoConverter mappingMongoConverter(ReactiveMongoDatabaseFactory databaseFactory,
-                                                       MongoCustomConversions customConversions, MongoMappingContext mappingContext) {
-
-        MappingMongoConverter converter=super.mappingMongoConverter(databaseFactory,customConversions,mappingContext);
-        DefaultMongoTypeMapper typeMapper = new DefaultMongoTypeMapper(null);
-        converter.setTypeMapper(typeMapper);
-
-        return converter;
-    }
 
     @Override
     protected boolean autoIndexCreation() {
