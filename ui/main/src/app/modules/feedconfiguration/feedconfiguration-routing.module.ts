7fe669206e5041ce2d4d4efff886ca144a7e0bf6
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-02-22 12:43:22 +0100

    Propose to save when exiting notification configuration screen (#5948)
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- ui/main/src/app/modules/feedconfiguration/feedconfiguration-routing.module.ts
+++ ui/main/src/app/modules/feedconfiguration/feedconfiguration-routing.module.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+/* Copyright (c) 2020-2024, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,13 +8,18 @@
  */
 
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {CanDeactivateFn, RouterModule, Routes} from '@angular/router';
 import {FeedconfigurationComponent} from './feedconfiguration.component';
+
+const canDeactivate: CanDeactivateFn<FeedconfigurationComponent> = (component: FeedconfigurationComponent) => {
+    return component.canDeactivate ? component.canDeactivate() : true;
+};
 
 const routes: Routes = [
     {
         path: '',
-        component: FeedconfigurationComponent
+        component: FeedconfigurationComponent,
+        canDeactivate: [canDeactivate]
     }
 ];
 
