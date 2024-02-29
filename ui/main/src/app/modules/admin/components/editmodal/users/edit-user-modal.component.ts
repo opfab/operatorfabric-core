07b0284a454afc254639dac685302e7ddf6b2cfc
ClementBouvierN <clement.bouvierneveu@rte-france.com>
2024-02-29 14:57:21 +0100

    Fixed typo in IP address regex
    Signed-off-by: ClementBouvierN <clement.bouvierneveu@rte-france.com>

--- ui/main/src/app/modules/admin/components/editmodal/users/edit-user-modal.component.ts
+++ ui/main/src/app/modules/admin/components/editmodal/users/edit-user-modal.component.ts
@@ -76,7 +76,9 @@ export class EditUserModalComponent implements OnInit {
             comment: new FormControl('', []),
             groups: new FormControl([]),
             entities: new FormControl([]),
-            authorizedIPAddresses: new FormControl('', [Validators.pattern(/^(?:\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3},?)+$/)])
+            authorizedIPAddresses: new FormControl('', [
+                Validators.pattern(/^(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3},?)+$/)
+            ])
         });
 
         if (this.row) {
