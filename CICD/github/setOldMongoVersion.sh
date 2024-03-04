b4281752a11d00a288931521e6c43fb2a89e26a6
freddidierRTE <frederic-f.didier@rte-france.com>
2024-03-04 17:59:22 +0100

    Set old supported version of mongo to 4.4
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- CICD/github/setOldMongoVersion.sh
+++ CICD/github/setOldMongoVersion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2022-2023, RTE (http://www.rte-france.com)
+# Copyright (c) 2022-2024, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,5 +8,5 @@
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of the OperatorFabric project.
  
-echo "Set mongo version to 4.0-bionic"
-sed -i "s/mongo:.*$/mongo:4.0-bionic/g" config/docker/docker-compose.yml
+echo "Set mongo version to 4.4-bionic"
+sed -i "s/mongo:.*$/mongo:4.4-bionic/g" config/docker/docker-compose.yml
