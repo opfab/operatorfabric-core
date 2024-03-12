fcdd5e695c27dbb9062bd063b7c31c5edf5c1737
freddidierRTE <frederic-f.didier@rte-france.com>
2024-03-12 15:32:17 +0100

    Add unit tests for angular translation service
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ ui/main/src/app/services/angularTranslationService.spec.ts
@@ -0,0 +1,58 @@
+/* Copyright (c) 2024, RTE (http://www.rte-france.com)
+ *  See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+import {TestBed} from '@angular/core/testing';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {AngularTranslationService} from './angularTranslationService';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('AngularTranslationService', () => {
+    let service: AngularTranslationService;
+    let translateService: TranslateService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, TranslateModule.forRoot()],
+            providers: [TranslateService]
+        });
+
+        translateService = TestBed.inject(TranslateService);
+        service = new AngularTranslationService(translateService);
+    });
+
+    it('should get translation', () => {
+        service.setTranslation('en', {key: 'valueInEnglish'}, true);
+        service.setLang('en');
+        expect(service.getTranslation('key')).toBe('valueInEnglish');
+    });
+
+    it('should get translation for chosen language', () => {
+        service.setTranslation('en', {key: 'value'}, true);
+        service.setTranslation('fr', {key: 'valueInFrench'}, true);
+        service.setLang('fr');
+        expect(service.getTranslation('key')).toBe('valueInFrench');
+    });
+
+    it('should get translation with a parameter', () => {
+        service.setTranslation('en', {key: 'valueInEnglish {{param}}'}, true);
+        service.setLang('en');
+        expect(service.getTranslation('key', {param: 'paramValue'})).toBe('valueInEnglish paramValue');
+    });
+
+    it('should get translation with two parameters', () => {
+        service.setTranslation('en', {key: 'valueInEnglish {{param1}} {{param2}}'}, true);
+        service.setLang('en');
+        expect(service.getTranslation('key', {param1: 'param1Value', param2: 'param2Value'})).toBe(
+            'valueInEnglish param1Value param2Value'
+        );
+    });
+
+    it('should get key name if key is not defined', () => {
+        expect(service.getTranslation('undefinedKey')).toBe('undefinedKey');
+    });
+});
