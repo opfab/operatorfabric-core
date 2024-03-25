1aa300e971ea64f0816b044a92064d8d0054d737
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-03-25 15:28:49 +0100

    Reduce Keycloak requests to improve configuration loading speed (#6176)
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- src/test/resources/getToken.sh
+++ src/test/resources/getToken.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2021-2023, RTE (http://www.rte-france.com)
+# Copyright (c) 2021-2024, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -26,11 +26,26 @@ then
 	password="test"
 fi
 
-echo "Get token for user $username on $url"
 
-access_token_pattern='"access_token":"([^"]+)"'
-response=$(curl -s -X POST -d "username="$username"&password="$password"&grant_type=password&client_id=opfab-client" $url:2002/auth/token)
-if [[ $response =~ $access_token_pattern ]] ; then
-	export token=${BASH_REMATCH[1]}
+
+getToken() {
+	echo "Get token for user $username on $url"
+	access_token_pattern='"access_token":"([^"]+)"'
+	response=$(curl -s -X POST -d "username="$username"&password="$password"&grant_type=password&client_id=opfab-client" $url:2002/auth/token)
+	if [[ $response =~ $access_token_pattern ]] ; then
+		export token=${BASH_REMATCH[1]}
+	fi
+	#echo  token=$token
+}
+
+if [ -n "${token}" ]; then
+	expire_date=`echo $token | jq -R 'split(".") | .[1] | @base64d | fromjson' | grep "exp" | cut -d ":" -f 2 | cut -d "," -f 1 | tr -d ' ' `
+
+	now=`date +%s`
+	if [ "$expire_date" -lt "$now" ]; then
+	  getToken
+	fi
+else
+    getToken
 fi
-#echo  token=$token
+
