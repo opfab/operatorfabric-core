760dbe1937ce1caf0f4cbdddcdca87ece80c2a6d
freddidierRTE <frederic-f.didier@rte-france.com>
2024-04-10 14:54:49 +0200

    Adjust code coverage configuration for node services cards-reminder and cards-external-diffusion
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- node-services/cards-external-diffusion/jest.config.js
+++ node-services/cards-external-diffusion/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
-    testEnvironment: 'node',
-  };
-  
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts}',
+        '!src/tests/**/*.{js,ts}', // Exclude test directory
+        '!src/domain/server-side/**/*.{js,ts}', // Exclude server-side directory that is tested via integration tests
+        '!src/cardsExternalDiffusion.ts' // Exclude cards external diffusion entry point that is tested via integration tests
+    ],
+    testEnvironment: 'node'
+};
--- node-services/cards-reminder/jest.config.js
+++ node-services/cards-reminder/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
-    testEnvironment: 'node',
-  };
-  
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts}',
+        '!src/tests/**/*.{js,ts}', // Exclude test directory
+        '!src/domain/server-side/**/*.{js,ts}', // Exclude server-side directory that is tested via integration tests
+        '!src/cardsReminder.ts' // Exclude cards reminder entry point that is tested via integration tests
+    ],
+    testEnvironment: 'node'
+};
