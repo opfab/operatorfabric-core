b9150b927a29933f01d24c4a699a7933816609fa
ClementBouvierN <clement.bouvierneveu@rte-france.com>
2024-04-11 16:42:55 +0200

    Updated script and file to send acknowledging request
    Signed-off-by: ClementBouvierN <clement.bouvierneveu@rte-france.com>

--- src/test/resources/cards/sendAckForCard.sh
+++ src/test/resources/cards/sendAckForCard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2022-2023, RTE (http://www.rte-france.com)
+# Copyright (c) 2022-2024, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,15 +8,24 @@
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of the OperatorFabric project.
 
+acknowledgingEntities=$3
 url=$4
+if [[ -z $acknowledgingEntities ]]
+then
+	acknowledgingEntities="acknowledgingEntities.json"
+fi
 if [[ -z $url ]]
 then
 	url="http://localhost"
 fi
-if [[ "$#" -lt 3 ]]
+if [[ -z $1 || -z $2 ]]
 then
-    echo "Usage : sendAckForCard user card_uid entitiesAcks opfab_url"
+    echo "Usage : sendAckForCard user card_uid acknowledgingEntities opfab_url"
 else
     source ../getToken.sh $url $1
-    curl -X POST $url:2102/cards/userAcknowledgement/$2 -H "Authorization: Bearer $token" -H "Content-type:application/json" --data $3
+    if [[ $acknowledgingEntities == *.json ]]; then
+        curl -X POST $url:2102/cards/userAcknowledgement/$2 -H "Authorization: Bearer $token" -H "Content-type:application/json" --data "$(envsubst <$acknowledgingEntities)"
+    else
+        curl -X POST $url:2102/cards/userAcknowledgement/$2 -H "Authorization: Bearer $token" -H "Content-type:application/json" --data $acknowledgingEntities
+    fi
 fi
