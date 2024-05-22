580cba218df58aecad8502779f7de7fe47db7b76
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-05-22 18:21:30 +0200

    Update node.js version in load_environment_light.sh
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -13,5 +13,5 @@ source ${BASH_SOURCE%/*}/load_variables.sh
 
 sdk install java 17.0.11-zulu
 sdk use java 17.0.11-zulu
-nvm install v20.12.2
-nvm use v20.12.2
+nvm install v20.13.1
+nvm use v20.13.1
