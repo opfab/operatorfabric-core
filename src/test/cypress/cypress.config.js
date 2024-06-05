b671e761a74da92eb0122322f162f0ec6524fefe
freddidierRTE <frederic-f.didier@rte-france.com>
2024-06-05 13:26:47 +0200

    Add higher resolution for cypress screenshots
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/cypress/cypress.config.js
+++ src/test/cypress/cypress.config.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2024, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -30,7 +30,39 @@ module.exports = defineConfig({
       on('task', {
         'readXlsx': readXlsx.read,
         'list': readXlsx.list,
-        'deleteFile': readXlsx.deleteFile})
+        'deleteFile': readXlsx.deleteFile},
+        on('before:browser:launch', (browser = {}, launchOptions) => {
+        
+          // the browser width and height we want to get
+          // our screenshots and videos will be of that resolution
+          const width = 1920
+          const height = 1080
+        
+          console.log('  Setting the browser window size to %d x %d', width, height)
+        
+          if (browser.name === 'chrome' && browser.isHeadless) {
+            launchOptions.args.push(`--window-size=${width},${height}`)
+        
+            // force screen to be non-retina and just use our given resolution
+            launchOptions.args.push('--force-device-scale-factor=1')
+          }
+        
+          if (browser.name === 'electron' && browser.isHeadless) {
+            // might not work on CI for some reason
+            launchOptions.preferences.width = width
+            launchOptions.preferences.height = height
+          }
+        
+          if (browser.name === 'firefox' && browser.isHeadless) {
+            launchOptions.args.push(`--width=${width}`)
+            launchOptions.args.push(`--height=${height}`)
+          }
+        
+          // IMPORTANT: return the updated browser launch options
+          return launchOptions
+        }))
     }
   }
 });
+
+
