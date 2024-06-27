977b205646a85bee465178074d645b0dc5970c1d
freddidierRTE <frederic-f.didier@rte-france.com>
2024-06-27 16:56:16 +0200

    Add ascii art on opfab-cli via docker
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- cli/entryPoint.sh
+++ cli/entryPoint.sh
@@ -8,6 +8,16 @@
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of the OperatorFabric project.
 
+echo -e ""
+echo -e "  ______   .______    _______    ___      .______                   ______  __       __  "
+echo -e " /  __  \  |   _  \  |   ____|  /   \     |   _  \                 /      ||  |     |  | "
+echo -e "|  |  |  | |  |_)  | |  |__    /  ^  \    |  |_)  |     ______    |  ,----'|  |     |  | "
+echo -e "|  |  |  | |   ___/  |   __|  /  /_\  \   |   _  <     |______|   |  |     |  |     |  | "
+echo -e "|  \`--'  | |  |      |  |    /  _____  \  |  |_)  |               |  \`----.|  \`----.|  | "
+echo -e " \______/  | _|      |__|   /__/     \__\ |______/                 \______||_______||__| "
+echo -e "                                                                                         "
+echo -e ""
+
 export HOME=/opfab
 # Add opfab-cli to the prompt in red
 export PS1="\033[31mOPFAB-CLI\033[0m \w \$ "
