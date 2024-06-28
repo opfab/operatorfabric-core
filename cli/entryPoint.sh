757ebcc20c6bea427e1e191cd6ec7b7293c4e2cf
freddidierRTE <frederic-f.didier@rte-france.com>
2024-06-28 15:50:56 +0200

    Provide a way to configure a list of environment for opfab docker cli
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- cli/entryPoint.sh
+++ cli/entryPoint.sh
@@ -19,7 +19,18 @@ echo -e "                                                                       
 echo -e ""
 
 export HOME=/opfab
-# Add opfab-cli to the prompt in red
-export PS1="\033[31mOPFAB-CLI\033[0m \w \$ "
+if [ -z "$ENV_NAME" ]; then
+    # Add opfab-cli to the prompt in red
+    export PS1="\033[32mOPFAB-CLI\033[0m \w \$ "
+else
+    export PS1="\033[32mOPFAB-CLI\033[0m - \033[31m$ENV_NAME\033[0m \w \$ "
+fi
+
+if [ -n "$OPFAB_URL" ]; then
+    opfab login $OPFAB_URL $OPFAB_PORT $OPFAB_LOGIN  $OPFAB_PASSWORD
+else
+    echo "No configuration provided. Please run 'opfab login' to login in to OperatorFabric."
+fi
+
 cd /opfab/host
 /bin/sh
--- cli/opfabDockerCli.sh
+++ cli/opfabDockerCli.sh
@@ -9,4 +9,33 @@
 # This file is part of the OperatorFabric project.
 
 
-docker run -v "$(pwd)":/opfab/host -u "$(id -u):$(id -g)" -it lfeoperatorfabric/of-opfab-cli:SNAPSHOT
+CONFIG_FILE=$1
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    docker run -v "$(pwd)":/opfab/host -u "$(id -u):$(id -g)" -it lfeoperatorfabric/of-opfab-cli:SNAPSHOT
+    exit
+fi
+
+# Display configurations and prompt the user to choose
+echo "Available configurations:"
+while IFS= read -r line; do
+    IFS=' ' read -r -a params <<< "$line"
+    echo " - ${params[0]}"
+done < "$CONFIG_FILE"
+echo "Enter the name of the configuration you want to use:"
+read CONFIG_NAME
+
+# Find the chosen configuration and set variables
+while IFS= read -r line; do
+    IFS=' ' read -r -a params <<< "$line"
+    if [[ "${params[0]}" == "$CONFIG_NAME" ]]; then
+        ENV_NAME="${params[1]}"
+        OPFAB_URL="${params[2]}"
+        OPFAB_PORT="${params[3]}"
+        OPFAB_LOGIN="${params[4]}"
+        OPFAB_PASSWORD="${params[5]}"
+        break
+    fi
+done < "$CONFIG_FILE"
+
+# Run the Docker command with the selected configuration
+docker run -v "$(pwd)":/opfab/host -u "$(id -u):$(id -g)" -it -e ENV_NAME="$ENV_NAME" -e OPFAB_URL="$OPFAB_URL" -e OPFAB_PORT="$OPFAB_PORT" -e OPFAB_LOGIN="$OPFAB_LOGIN" -e OPFAB_PASSWORD="$OPFAB_PASSWORD" lfeoperatorfabric/of-opfab-cli:SNAPSHOT
