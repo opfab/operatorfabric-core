0622a84c8eb4082a6df9ca8d65db80d4b5922a06
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-07-16 11:41:36 +0200

    Update load_environment_light.sh to use node.js v20.15.1
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -13,8 +13,8 @@ source ${BASH_SOURCE%/*}/load_variables.sh
 
 sdk install java 17.0.11-zulu
 sdk use java 17.0.11-zulu
-nvm install v20.15.0
-nvm use v20.15.0
+nvm install v20.15.1
+nvm use v20.15.1
 
 (
     echo "Install opfab-cli"
