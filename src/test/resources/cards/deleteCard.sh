cf809bde47a058f094100dd63d367a2b779e92d1
vlo-rte <valerie.longa@rte-france.com>
2024-07-19 17:04:14 +0200

    Use cli in deleteCard.sh (#6839)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- src/test/resources/cards/deleteCard.sh
+++ src/test/resources/cards/deleteCard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2021-2023, RTE (http://www.rte-france.com)
+# Copyright (c) 2021-2024, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,10 +13,17 @@ if [[ -z $url ]]
 then
 	url="http://localhost"
 fi
+
+port=$3
+if [[ -z $port ]]
+then
+	port=2002
+fi
+
 if [[ -z $1 ]]
 then
     echo "Usage : deleteCard card_id opfab_url"
 else
-    source ../getToken.sh $url
-    curl -s -X DELETE $url:2102/cards/$1 -H "Authorization: Bearer $token" -H "Content-type:application/json"
+    opfab login $url $port admin test
+    opfab card delete $1
 fi
