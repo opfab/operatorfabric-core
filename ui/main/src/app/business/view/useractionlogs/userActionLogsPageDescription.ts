3030165d98b2f475f8ac8c3a134ae0d93cb12d89
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-07-24 11:35:28 +0200

    User action logs: add CLOSE_SUBSCRIPTION in action filter (#6854)
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- ui/main/src/app/business/view/useractionlogs/userActionLogsPageDescription.ts
+++ ui/main/src/app/business/view/useractionlogs/userActionLogsPageDescription.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2024, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -19,7 +19,8 @@ export class UserActionLogsPageDescription {
         'UNREAD_CARD',
         'SEND_CARD',
         'SEND_RESPONSE',
-        'OPEN_SUBSCRIPTION'
+        'OPEN_SUBSCRIPTION',
+        'CLOSE_SUBSCRIPTION'
     ];
     columnTitle: {
         date: string;
