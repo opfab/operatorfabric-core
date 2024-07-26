2e506dbd8f23d95c9df2ea3aab32daa8cf085b5d
Giovanni Ferrari <giovanni.ferrari@soft.it>
2024-07-26 11:16:14 +0200

    Update jdk version to 17.0.12-zulu in load_environment_light.sh
    Signed-off-by: Giovanni Ferrari <giovanni.ferrari@soft.it>

--- bin/load_environment_light.sh
+++ bin/load_environment_light.sh
@@ -11,8 +11,8 @@
 
 source ${BASH_SOURCE%/*}/load_variables.sh
 
-sdk install java 17.0.11-zulu
-sdk use java 17.0.11-zulu
+sdk install java 17.0.12-zulu
+sdk use java 17.0.12-zulu
 nvm install v20.15.1
 nvm use v20.15.1
 
