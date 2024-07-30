8a82e42f5b1a638641b0cbe6095064699096dbf4
vlo-rte <valerie.longa@rte-france.com>
2024-07-30 15:30:34 +0200

    Use opfab-cli "commands" feature in createAllPerimeter.sh (#6917)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- src/test/resources/perimeters/createAllPerimeter.sh
+++ src/test/resources/perimeters/createAllPerimeter.sh
@@ -9,30 +9,5 @@
 # This file is part of the OperatorFabric project.
 
 
-opfab perimeters create *.json
-opfab perimeters addtogroup question Dispatcher
-opfab perimeters addtogroup question Planner
-opfab perimeters addtogroup defaultProcess Dispatcher
-opfab perimeters addtogroup defaultProcess Planner
-opfab perimeters addtogroup conferenceAndITIncidentExample Dispatcher
-opfab perimeters addtogroup conferenceAndITIncidentExample Planner
-opfab perimeters addtogroup conferenceAndITIncidentExample Supervisor
-opfab perimeters addtogroup messageOrQuestionExample Dispatcher
-opfab perimeters addtogroup messageOrQuestionExample Planner
-opfab perimeters addtogroup messageOrQuestionExample Supervisor
-opfab perimeters addtogroup taskExample Dispatcher
-opfab perimeters addtogroup taskExample Planner
-opfab perimeters addtogroup taskExample Supervisor
-opfab perimeters addtogroup taskAdvancedExample Dispatcher
-opfab perimeters addtogroup taskAdvancedExample Planner
-opfab perimeters addtogroup taskAdvancedExample Supervisor
-opfab perimeters addtogroup gridCooperation Dispatcher
-opfab perimeters addtogroup gridCooperation Planner
-opfab perimeters addtogroup gridCooperation Supervisor
-opfab perimeters addtogroup cypress Dispatcher
-opfab perimeters addtogroup cypress Planner
-opfab perimeters addtogroup cypress Supervisor
-opfab perimeters addtogroup questionReceiveOnly Supervisor
-opfab perimeters addtogroup externalRecipient Dispatcher
-opfab perimeters addtogroup supervisor Dispatcher
+opfab commands createAllPerimeter.txt
  
