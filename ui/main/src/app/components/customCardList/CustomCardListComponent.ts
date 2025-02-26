5105d7aca8c33d56ad99bbbf4593e514870b04cf
vlo-rte <valerie.longa@rte-france.com>
2025-02-26 14:12:50 +0100

    Add loading spinner in custom card list screen (#7904)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- ui/main/src/app/components/customCardList/CustomCardListComponent.ts
+++ ui/main/src/app/components/customCardList/CustomCardListComponent.ts
@@ -10,7 +10,7 @@
 import {AsyncPipe, NgFor, NgIf} from '@angular/common';
 import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgbModal, NgbModalOptions, NgbModalRef, NgbPagination} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, NgbModalOptions, NgbModalRef, NgbPagination, NgbPopover} from '@ng-bootstrap/ng-bootstrap';
 import {TranslateModule} from '@ngx-translate/core';
 import {SelectedCardService} from '@ofServices/selectedCard/SelectedCardService';
 import {TranslationService} from '@ofServices/translation/TranslationService';
@@ -30,6 +30,8 @@ import {TypeOfStateEnum} from '@ofServices/processes/model/Processes';
 import {HasResponseCellRendererComponent} from './cellRenderers/HasResponseCellRendererComponent';
 import {InputCellRendererComponent} from './cellRenderers/InputCellRendererComponent';
 import {AgGrid} from 'app/utils/AgGrid';
+import {OpfabEventStreamService} from '@ofServices/events/OpfabEventStreamService';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
     selector: 'of-custom-card-list-screen',
@@ -47,7 +49,8 @@ import {AgGrid} from 'app/utils/AgGrid';
         ReactiveFormsModule,
         NgbPagination,
         CardComponent,
-        MultiSelectComponent
+        MultiSelectComponent,
+        NgbPopover
     ]
 })
 export class CustomCardListComponent implements OnInit, OnDestroy {
@@ -117,6 +120,8 @@ export class CustomCardListComponent implements OnInit, OnDestroy {
 
     private readonly ngUnsubscribe$ = new Subject<void>();
 
+    loadingInProgress = false;
+
     constructor(private readonly modalService: NgbModal) {
         ModuleRegistry.registerModules([AllCommunityModule]);
         provideGlobalGridOptions({theme: 'legacy'});
@@ -125,6 +130,12 @@ export class CustomCardListComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
+        OpfabEventStreamService.getLoadingInProgress()
+            .pipe(takeUntil(this.ngUnsubscribe$), debounceTime(500))
+            .subscribe((loadingInProgress: boolean) => {
+                this.loadingInProgress = loadingInProgress;
+            });
+
         this.customCardListView = new CustomCardListView(this.customScreenId);
 
         const severityCellClassRules = {
