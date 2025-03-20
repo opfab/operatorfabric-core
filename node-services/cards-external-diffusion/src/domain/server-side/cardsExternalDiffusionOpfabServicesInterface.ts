40e72067af613cffa32f3a95fcf892d195da5560
freddidierRTE <frederic-f.didier@rte-france.com>
2025-03-20 15:41:44 +0100

    Solve sonar issue in node services
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- node-services/cards-external-diffusion/src/domain/server-side/cardsExternalDiffusionOpfabServicesInterface.ts
+++ node-services/cards-external-diffusion/src/domain/server-side/cardsExternalDiffusionOpfabServicesInterface.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -29,7 +29,7 @@ export default class CardsExternalDiffusionOpfabServicesInterface
         this.listener = new EventBusListener().addListener(this);
     }
 
-    async onMessage(message: any): Promise<void> {
+    async onMessage(message: any) {
         const username: string = message.content.toString();
         this.logger.info('EventBusListener received user update event: ' + username);
         if (username != null && username.length > 0) await this.clearCacheForUser(username);
