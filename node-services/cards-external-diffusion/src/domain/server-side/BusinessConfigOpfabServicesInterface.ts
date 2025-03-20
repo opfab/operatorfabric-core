c964151c46fcaba280fee073e38718a0548a51f2
freddidierRTE <frederic-f.didier@rte-france.com>
2025-03-20 15:41:44 +0100

    Solve sonar issue in node services
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- node-services/cards-external-diffusion/src/domain/server-side/BusinessConfigOpfabServicesInterface.ts
+++ node-services/cards-external-diffusion/src/domain/server-side/BusinessConfigOpfabServicesInterface.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2024-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -28,7 +28,7 @@ export default class BusinessConfigOpfabServicesInterface extends OpfabServicesI
         MailHandlebarsHelper.init();
     }
 
-    async onMessage(message: any): Promise<void> {
+    async onMessage(message: any) {
         const businessconfigMessage: string = message.content.toString();
         this.logger.info('EventBusListener received businessconfig update event: ' + businessconfigMessage);
         if (businessconfigMessage === 'BUSINESS_CONFIG_CHANGE') {
--- node-services/cards-reminder/src/domain/application/reminderService.ts
+++ node-services/cards-reminder/src/domain/application/reminderService.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -28,7 +28,7 @@ export default class ReminderService implements EventListener {
         return this;
     }
 
-    async onMessage(message: any): Promise<void> {
+    async onMessage(message: any) {
         try {
             const cardOperation: CardOperation = JSON.parse(message.content as string);
 
