71436b232a6ec7ff33f2215a18c8a3f7653150ae
freddidierRTE <frederic-f.didier@rte-france.com>
2025-04-01 15:35:44 +0200

    Change color of answer in card detail to be coherent with custom screen (#8144)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/cypress/cypress/support/cardCommands.js
+++ src/test/cypress/cypress/support/cardCommands.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022-2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2022-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,11 +7,9 @@
  * This file is part of the OperatorFabric project.
  */
 
-
 import {OpfabCommands} from './opfabCommands';
 
 export class CardCommands extends OpfabCommands {
-
     constructor() {
         super();
         super.init('CARD');
@@ -19,74 +17,80 @@ export class CardCommands extends OpfabCommands {
 
     acknowledge = function () {
         cy.get('#opfab-card-details-btn-ack').click();
-    }
+    };
 
     checkEditButtonDoesNotExist = function () {
         cy.get('#opfab-card-edit').should('not.exist');
-    }
+    };
 
     checkCopyButtonDoesNotExist = function () {
         cy.get('#opfab-card-create-copy').should('not.exist');
-    }
+    };
 
     checkCopyButtonDoesExist = function () {
         cy.get('#opfab-card-create-copy').should('exist');
-    }
+    };
 
     checkDeleteButtonDoesNotExist = function () {
         cy.get('#opfab-card-delete').should('not.exist');
-    }
+    };
 
-    checkEntityIsGreenInCardHeader = function(entityId) {
+    checkEntityIsRedInCardHeader = function (entityId) {
+        cy.get(`#opfab-card-header-entity-${entityId}`).should('have.css', 'color', 'rgb(255, 0, 0)');
+    };
+
+    checkEntityIsBlueInCardHeader = function (entityId) {
+        cy.get(`#opfab-card-header-entity-${entityId}`).should('have.css', 'color', 'rgb(39, 132, 255)');
+    };
+
+    checkEntityIsGreenInCardHeader = function (entityId) {
         cy.get(`#opfab-card-header-entity-${entityId}`).should('have.css', 'color', 'rgb(0, 128, 0)');
-    }
+    };
 
-    checkEntityIsOrangeInCardHeader = function(entityId) {
-        cy.get(`#opfab-card-header-entity-${entityId}`).should('have.css', 'color', 'rgb(255, 102, 0)');
-    }
+    checkEntityIsGreyInCardHeader = function (entityId) {
+        cy.get(`#opfab-card-header-entity-${entityId}`).should('have.css', 'color', 'rgb(108, 117, 125)');
+    };
 
-    checkEntityIsNotVisibleInCardHeader = function(entityId) {
+    checkEntityIsNotVisibleInCardHeader = function (entityId) {
         cy.get(`#opfab-card-header-entity-${entityId}`).should('not.exist');
-    }
+    };
 
     checkExpirationDateIconDoesExist = function () {
         cy.get('#opfab-card-expiration-date').should('exist');
-    }
+    };
 
-    close= function () {
+    close = function () {
         cy.get('#opfab-close-card').click();
-    }
+    };
 
     delete = function () {
         cy.get('#opfab-card-delete').click();
         cy.get('#opfab-btn-ok').click();
-    }
+    };
 
     modifyResponse = function () {
         cy.get('#opfab-card-details-btn-response').click();
-    }
+    };
 
     openEntityDropdownInCardHeader() {
         cy.get('#opfab-card-response-header-right').click();
     }
 
-    clickOnSendResponse = function() {
+    clickOnSendResponse = function () {
         cy.get('#opfab-card-details-btn-response').click();
-    }
+    };
 
-    closeMessageAfterResponseSend = function() {
+    closeMessageAfterResponseSend = function () {
         cy.get('.opfab-info-message').contains('Your answer is confirmed. Thank you!');
         cy.get('#opfab-close-alert').click();
-    }
+    };
 
     sendResponse = function () {
         this.clickOnSendResponse();
         this.closeMessageAfterResponseSend();
-    }
-
+    };
 
     unacknowledge = function () {
         cy.get('#opfab-card-details-btn-unack').click();
-    }
+    };
 }
-
