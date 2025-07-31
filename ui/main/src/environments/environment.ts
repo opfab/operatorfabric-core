32f52e52782bf72a0e2b29ec658014a2a674932b
freddidierRTE <frederic-f.didier@rte-france.com>
2025-07-31 13:27:06 +0200

    Solve sonar issue "Remove this commented out code"
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- ui/main/src/environments/environment.ts
+++ ui/main/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2018-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -15,11 +15,3 @@ export const environment = {
     production: false,
     url: 'http://localhost:2002/'
 };
-
-/*
- * In development mode, to ignore zone related message stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw message
- */
-// import 'zone.js/plugins/zone-message';  // Included with Angular CLI.
