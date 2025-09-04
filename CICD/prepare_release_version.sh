7551d6b7ba79681365c952dcdaf2c4b9d4b6297b
freddidierRTE <frederic-f.didier@rte-france.com>
2025-09-04 14:57:16 +0200

    Solve issue in prepare release version script
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- CICD/prepare_release_version.sh
+++ CICD/prepare_release_version.sh
@@ -65,8 +65,7 @@ find . -name swagger.yaml | xargs sed -i "s/\(version: *\)$oldVersion/\1$newVers
 
 echo "Using $newVersion for lfeoperatorfabric images in dev and docker environment docker compose files"
 # String example for regexp: image: "lfeoperatorfabric/of-web-ui:0.13.1.RELEASE"
-sed -i "s/\( *image *: *\"lfeoperatorfabric\/.*:\)\(.*\)\"/\1$newVersion\"/g" ./config/docker/docker-compose.yml;
-sed -i "s/\( *image *: *\"lfeoperatorfabric\/.*:\)\(.*\)\"/\1$newVersion\"/g" ./config/dev/docker-compose.yml;
+sed -i "s/\( *image *: *\"lfeoperatorfabric\/.*:\)\(.*\)\"/\1$newVersion\"/g" ./config/docker-compose.yml;
 
 echo "Using $newVersion for lfeoperatorfabric/of-opfab-cli"
 sed -i "s/lfeoperatorfabric\/of-opfab-cli:.*/lfeoperatorfabric\/of-opfab-cli:$newVersion/" ./cli/opfabDockerCli.sh;
