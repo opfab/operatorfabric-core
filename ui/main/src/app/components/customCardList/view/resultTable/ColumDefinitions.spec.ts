0e70bcb89dde266e0738bdc31b45e27132fc742f
freddidierRTE <frederic-f.didier@rte-france.com>
2025-09-24 18:13:49 +0200

    Custom Screen refacto: isolate AgGrid column definitions in one file (ColumnDefinitions.ts)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ ui/main/src/app/components/customCardList/view/resultTable/ColumDefinitions.spec.ts
@@ -0,0 +1,221 @@
+/* Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+import {CustomScreenDefinition, FieldType} from '@ofServices/customScreen/model/CustomScreenDefinition';
+import {getColumnsDefinitionForAgGrid} from './ColumnDefinitions';
+
+function createCustomScreenDefinition(columns: any[]): CustomScreenDefinition {
+    return {
+        id: 'testId',
+        name: 'testName',
+        headerFilters: [],
+        processIds: [],
+        results: {
+            columns: columns
+        }
+    };
+}
+describe('CustomScreenView - ResultTable', () => {
+    describe('Should get columns definition for ag-grid', () => {
+        it('with default column definition', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Process',
+                        cardField: 'processId'
+                    }
+                ])
+            );
+            expect(columnDefs.length).toBe(1);
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('Process');
+            expect(columnDefs[0].type).toBe('default');
+            expect(columnDefs[0].sortable).toBeTrue();
+            expect(columnDefs[0].filter).toBeTrue();
+        });
+
+        it('columnDefinition type is STRING', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Process',
+                        cardField: 'processId',
+                        fieldType: FieldType.STRING,
+                        flex: 2
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('Process');
+            expect(columnDefs[0].type).toBe('default');
+            expect(columnDefs[0].flex).toBe(2);
+        });
+
+        it('when columnDefinition type is DATE_AND_TIME ', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Start Date',
+                        cardField: 'startDate',
+                        fieldType: FieldType.DATE_AND_TIME
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('Start Date');
+            expect(columnDefs[0].type).toBe('dateAndTime');
+        });
+
+        it('when columnDefinition type is HTML', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'HTML',
+                        cardField: 'htmlField',
+                        fieldType: FieldType.HTML
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('HTML');
+            expect(columnDefs[0].type).toBe('html');
+        });
+
+        it('when columnDefinition type is coloredCircle', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Circle',
+                        cardField: 'circleField',
+                        fieldType: FieldType.COLORED_CIRCLE
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('Circle');
+            expect(columnDefs[0].type).toBe('coloredCircle');
+        });
+
+        it('when columnDefinition type is BUSINESS_PERIOD', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Business Period',
+                        cardField: 'businessPeriod',
+                        fieldType: FieldType.BUSINESS_PERIOD
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('testField');
+            expect(columnDefs[0].headerName).toBe('Business Period');
+            expect(columnDefs[0].type).toBe('period');
+            expect(columnDefs[0].autoHeight).toBeTrue(); // the business period is displayed on two lines
+            expect(columnDefs[0].wrapText).toBeTrue();
+        });
+
+        it('when columnDefinition type is SEVERITY', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'severity',
+                        cardField: 'processId',
+                        fieldType: FieldType.SEVERITY
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('severity');
+            expect(columnDefs[0].headerName).toBe('');
+            expect(columnDefs[0].type).toBe('severity');
+            expect(columnDefs[0].filter).toBeFalse();
+        });
+
+        it('when columnDefinition type is TYPE_OF_STATE', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        headerName: 'Status',
+                        fieldType: FieldType.TYPE_OF_STATE
+                    }
+                ])
+            );
+            expect(columnDefs[0].field).toBe('typeOfState');
+            expect(columnDefs[0].headerName).toBe('Status');
+            expect(columnDefs[0].type).toBe('typeOfState');
+        });
+
+        it('when columnDefinition showTooltips is set to true', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'test',
+                        headerName: 'Status',
+                        fieldType: FieldType.STRING,
+                        showTooltips: true
+                    }
+                ])
+            );
+            expect(columnDefs[0].tooltipValueGetter).toBeDefined();
+        });
+
+        it('when minWidth is defined in column definition', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        field: 'testField',
+                        headerName: 'Process',
+                        cardField: 'processId',
+                        fieldType: FieldType.STRING,
+                        minWidth: 200
+                    },
+                    {
+                        field: 'testField2',
+                        headerName: 'test2',
+                        fieldType: FieldType.STRING,
+                        cardField: 'test2'
+                    }
+                ])
+            );
+            expect(columnDefs[0].minWidth).toBe(200);
+            expect(columnDefs[1].minWidth).toBeUndefined();
+        });
+
+        it('when columnDefinition with multiLineText to true then autoHeight and wrapText are true', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        headerName: 'Status',
+                        fieldType: FieldType.TYPE_OF_STATE,
+                        multiLinesInCell: true
+                    }
+                ])
+            );
+            expect(columnDefs[0].autoHeight).toBeTrue();
+            expect(columnDefs[0].wrapText).toBeTrue();
+        });
+
+        it('when columnDefinition maxInputLength is set when maxInputLength is set', () => {
+            const columnDefs = getColumnsDefinitionForAgGrid(
+                createCustomScreenDefinition([
+                    {
+                        headerName: 'Status',
+                        fieldType: FieldType.TYPE_OF_STATE,
+                        maxInputLength: 200
+                    }
+                ])
+            );
+            expect(columnDefs[0].context.maxInputLength).toBe(200);
+        });
+    });
+});
