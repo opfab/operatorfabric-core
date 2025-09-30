b2b83bddf40446aaf2a5307b4d5a94b54cad345e
freddidierRTE <frederic-f.didier@rte-france.com>
2025-09-30 17:40:45 +0200

    Permit to load custom handlebars helpers for emails (#8917)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

---
+++ config/services/customHandlebarsHelpersExample.js
@@ -0,0 +1,35 @@
+/* Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the OperatorFabric project.
+ */
+
+// This is an example file
+// Do not use in production
+
+function example_severityColor(severity) {
+    let color;
+    switch (severity) {
+        case 'ALARM':
+            color = 'red';
+            break;
+        case 'ACTION':
+            color = 'orange';
+            break;
+        case 'COMPLIANT':
+            color = 'green';
+            break;
+        case 'INFORMATION':
+            color = 'blue';
+            break;
+        default:
+            color = 'white';
+            break;
+    }
+    return color;
+}
+
+helpers = [example_severityColor];
