d9e52f21ac469fedbd5f691c5a3b58389f77c6bc
freddidierRTE <frederic-f.didier@rte-france.com>
2025-10-14 16:13:15 +0200

    Move spring-utilities lib in services/common
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- src/test/externalApp/src/main/java/org/opfab/externalapp/config/JacksonConfig.java
+++ src/test/externalApp/src/main/java/org/opfab/externalapp/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022-2024, RTE (http://www.rte-france.com)
+/* Copyright (c) 2022-2025, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,7 +10,6 @@
 package org.opfab.externalapp.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.opfab.springtools.json.InstantModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -18,11 +17,10 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 @Configuration
 public class JacksonConfig {
 
-  @Bean
-  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-    ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-    objectMapper.registerModule(new InstantModule());
-    return objectMapper;
-  }
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.registerModule(new InstantModule());
+        return objectMapper;
+    }
 }
-
