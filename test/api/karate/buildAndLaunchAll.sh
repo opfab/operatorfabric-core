0ca3a256c9948e12f66b8dd2ad87fff56a3cd320
freddidierRTE <frederic-f.didier@rte-france.com>
2025-10-20 16:45:03 +0200

    Move test environment docker definition in test/
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- test/api/karate/buildAndLaunchAll.sh
+++ test/api/karate/buildAndLaunchAll.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-# Copyright (c) 2018-2024, RTE (http://www.rte-france.com)
+# Copyright (c) 2018-2025, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of the OperatorFabric project.
 
-dockerComposeFile=src/main/docker/test-environment/docker-compose.yml
+dockerComposeFile=test/test-environment/docker-compose.yml
 (cd ../../../
 echo "Stop java services"
 bin/run_all.sh stop
