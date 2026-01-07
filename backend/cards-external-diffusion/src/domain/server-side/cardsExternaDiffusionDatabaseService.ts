2ac5ecd55adba719797c3c97189234eaff3d0579
vlo-rte <valerie.longa@rte-france.com>
2026-01-07 18:24:14 +0100

    suppression objet CardProjection et ajout data
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- backend/cards-external-diffusion/src/domain/server-side/cardsExternaDiffusionDatabaseService.ts
+++ backend/cards-external-diffusion/src/domain/server-side/cardsExternaDiffusionDatabaseService.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2026, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -113,6 +113,7 @@ export default class CardsExternalDiffusionDatabaseService {
                     publisher: 1,
                     publisherType: 1,
                     severity: 1,
+                    data: 1,
                     _id: 0
                 })
                 .toArray();
