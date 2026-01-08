35a82e551984ea6bab45df601106136b388a720f
freddidierRTE <frederic-f.didier@rte-france.com>
2026-01-08 19:09:55 +0100

    Implement code flow with angular oidc lib (#9188)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- frontend/src/app/authentication/CodeAuthenticationHandler.ts
+++ frontend/src/app/authentication/CodeAuthenticationHandler.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2026, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,38 +7,108 @@
  * This file is part of the OperatorFabric project.
  */
 
-import {Observable} from 'rxjs';
-import {AuthHandler, HttpAuthInfo} from './AuthHandler';
-import {HttpHeaders} from '@angular/common/http';
-import {environment} from '@env/environment';
+import {AuthHandler} from './AuthHandler';
+import {HttpClient} from '@angular/common/http';
+import {AuthConfig, EventType, JwksValidationHandler, OAuthEvent, OAuthService} from 'angular-oauth2-oidc';
+import {CurrentUserStore} from '@ofStore/CurrentUserStore';
+import {AuthenticatedUser} from './AuthModel';
+import {LoggerService} from '@ofServices/logs/LoggerService';
+import {ConfigService} from '@ofServices/config/ConfigService';
 
 export class CodeAuthenticationHandler extends AuthHandler {
-    initializeAuthentication() {
-        let authCode;
-        const searchCodeString = 'code=';
-        const foundIndex = globalThis.location.href.indexOf(searchCodeString);
-        if (foundIndex === -1) {
+    constructor(
+        httpClient: HttpClient,
+        private readonly oauthService: OAuthService
+    ) {
+        super(httpClient);
+    }
+
+    public async initializeAuthentication() {
+        const authConfig: AuthConfig = {
+            issuer: this.delegateUrl,
+            redirectUri: location.href,
+            silentRefreshRedirectUri: `${location.origin}${location.pathname}silent-refresh.html`,
+            clientId: this.clientId,
+            scope: 'openid profile email',
+            showDebugInformation: false,
+            sessionChecksEnabled: false,
+            clearHashAfterLogin: false,
+            requireHttps: false,
+            useSilentRefresh: ConfigService.getConfigValue('security.oauth2.useSilentRefresh', false),
+            postLogoutRedirectUri: ConfigService.getConfigValue(
+                'security.post-logout-url',
+                `${location.origin}${location.pathname}`
+            )
+        };
+        // In the code flow, passing a hash in the redirect_uri is not possible, so we store the route before login.
+        // Only store the route if this is not the redirect back from the auth server (i.e., after login).
+        // We detect a redirect back from the auth server if 'state=' is present in the URL.
+        // In case a url contains 'state=' without being a redirect from the auth server, the route will not be saved,
+        // and the user will be redirected to '/' after login (which is acceptable).
+        if (!globalThis.location.href.includes('state=')) {
             this.saveOpfabRoute();
-        } else {
-            authCode = globalThis.location.href.substring(foundIndex + searchCodeString.length);
         }
 
-        this.checkAuthentication().subscribe((token) => {
-            // no token stored or token invalid
-            if (token && this.isTokenStillValid()) {
-                this.userAuthenticated.next(null);
-            } else if (authCode) {
-                this.askToken(authCode).subscribe({
-                    next: (authInfo) => this.userAuthenticated.next(this.getUserFromAuthInfo(authInfo)),
-                    error: (error) => this.handleErrorOnTokenGeneration(error, 'code')
-                });
+        this.oauthService.configure(authConfig);
+        this.oauthService.setupAutomaticSilentRefresh();
+        this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+        await this.oauthService.loadDiscoveryDocument().then(() => {
+            this.login();
+        });
+        this.oauthService.events.subscribe((e) => {
+            this.dispatchOAuth2Events(e);
+        });
+    }
+
+    private async login() {
+        await this.oauthService.tryLogin().then(() => {
+            if (this.oauthService.hasValidAccessToken()) {
+                this.setUserAuthenticated();
+                this.updateAfterSilentRefresh();
             } else {
-                this.moveToLoginPage();
+                this.oauthService.initCodeFlow();
             }
         });
     }
-    // With the code flow it is not possible to pass a hash in the redirect_uri
-    // so we need to save in the session storage the route before login.
+
+    // hack to update token as silent refresh updates the token in background
+    // we need to update regularly as we do not catch when refresh is done
+    updateAfterSilentRefresh() {
+        setInterval(() => {
+            const token = this.oauthService.getAccessToken();
+            const expirationDate = new Date(this.oauthService.getAccessTokenExpiration());
+            localStorage.setItem('token', token);
+            localStorage.setItem('expirationDate', expirationDate?.getTime().toString());
+            CurrentUserStore.setToken(token);
+        }, 5000);
+    }
+
+    private setUserAuthenticated() {
+        const user = new AuthenticatedUser();
+        const identityClaims = this.oauthService.getIdentityClaims();
+        user.login = identityClaims[this.loginClaim];
+        user.token = this.oauthService.getAccessToken();
+        user.expirationDate = new Date(this.oauthService.getAccessTokenExpiration());
+        this.userAuthenticated.next(user);
+    }
+
+    private dispatchOAuth2Events(event: OAuthEvent) {
+        const eventType: EventType = event.type;
+        switch (eventType) {
+            // We can have a token_error or token_refresh_error when it is not possible to refresh token
+            // This case arise for example when using a SSO and the session is not valid anymore (session timeout)
+            case 'token_error':
+            case 'token_refresh_error':
+                this.tokenWillSoonExpire.next(true);
+                this.tokenExpired.next(true);
+                break;
+            case 'logout': {
+                LoggerService.info('Logout from code flow');
+                break;
+            }
+        }
+    }
+    // We save in the session storage the route before login.
     // We do not use the local storage because the local storage is shared between tabs
     private saveOpfabRoute() {
         const hash = globalThis.location.hash;
@@ -51,26 +121,7 @@ export class CodeAuthenticationHandler extends AuthHandler {
         return globalThis.sessionStorage.getItem('route_after_login_for_code_flow');
     }
 
-    private askToken(code: string): Observable<HttpAuthInfo> {
-        const params = new URLSearchParams();
-        params.append('code', code);
-        params.append('grant_type', 'authorization_code');
-        params.append('clientId', this.clientId);
-        params.append('redirect_uri', this.getRedirectUri());
-
-        const headers = new HttpHeaders({'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'});
-        return this.httpClient.post<HttpAuthInfo>(this.askTokenUrl, params.toString(), {headers: headers});
-    }
-
-    private moveToLoginPage() {
-        if (this.delegateUrl) {
-            globalThis.location.href = `${this.delegateUrl}&redirect_uri=${this.getRedirectUri()}`;
-        } else {
-            globalThis.location.href = `${environment.url}auth/code/redirect_uri=${this.getRedirectUri()}`;
-        }
-    }
-
-    private getRedirectUri(): string {
-        return `${location.origin}${location.pathname}`;
+    public logout() {
+        this.oauthService.logOut();
     }
 }
