e7991a2825c6790f54175db082eeb9a1816bc101
vlo-rte <valerie.longa@rte-france.com>
2026-01-30 15:04:52 +0100

    Add a more meaningfull error when try to download a none json file as business data (#8986)
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- frontend/src/app/services/businessdata/businessdata.service.ts
+++ frontend/src/app/services/businessdata/businessdata.service.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
+/* Copyright (c) 2023-2026, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -114,7 +114,12 @@ export class BusinessDataService {
                     );
                     AlertMessageService.sendAlertMessage({
                         message: '',
-                        i18n: {key: 'shared.error.businessData.updateBusinessData'},
+                        i18n: {
+                            key: 'shared.error.businessData.updateBusinessData',
+                            parameters: {
+                                reason: responseBusinessData.statusMessage || 'unknown error'
+                            }
+                        },
                         level: MessageLevel.ERROR
                     });
                     return null;
