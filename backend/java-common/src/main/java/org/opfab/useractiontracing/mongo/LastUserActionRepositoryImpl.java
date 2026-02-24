de804fbda8b67130d63ff3ca77532e362f7079ae
freddidierRTE <frederic-f.didier@rte-france.com>
2026-02-24 17:11:43 +0100

    Opfab CLI : offer more options for command "user last-user-action" (#9662)
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- backend/java-common/src/main/java/org/opfab/useractiontracing/mongo/LastUserActionRepositoryImpl.java
+++ backend/java-common/src/main/java/org/opfab/useractiontracing/mongo/LastUserActionRepositoryImpl.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2025, RTE (http://www.rte-france.com)
+/* Copyright (c) 2025-2026, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,6 +13,7 @@ import org.opfab.useractiontracing.model.LastUserAction;
 import org.opfab.useractiontracing.repositories.LastUserActionRepository;
 import org.springframework.data.domain.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +39,11 @@ public class LastUserActionRepositoryImpl implements LastUserActionRepository {
     @Override
     public Optional<LastUserAction> findById(String id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public List<LastUserAction> findByLastActionDateBefore(Instant date) {
+        return repository.findByLastActionDateBefore(date);
     }
 
 }
