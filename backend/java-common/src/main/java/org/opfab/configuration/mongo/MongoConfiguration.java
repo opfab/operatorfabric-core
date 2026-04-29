49e83f73915a19b559e795afa154566aee9d484b
freddidierRTE <frederic-f.didier@rte-france.com>
2026-04-29 09:56:52 +0200

    Upgrade to springboot 4.0.6
    Signed-off-by: freddidierRTE <frederic-f.didier@rte-france.com>

--- backend/java-common/src/main/java/org/opfab/configuration/mongo/MongoConfiguration.java
+++ backend/java-common/src/main/java/org/opfab/configuration/mongo/MongoConfiguration.java
@@ -11,11 +11,17 @@ package org.opfab.configuration.mongo;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import org.springframework.boot.mongodb.autoconfigure.MongoProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
@@ -47,6 +53,38 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return mongoProperties.getDatabase();
+    }
+
+    /**
+     * Provides an imperative MongoTemplate bean required
+     * by @EnableMongoRepositories.
+     * This is necessary because MongoConfiguration extends
+     * AbstractReactiveMongoConfiguration
+     * which only provides reactive beans; imperative MongoRepository interfaces
+     * need a MongoTemplate backed by an imperative MongoClient.
+     * The MappingMongoConverter is injected so that any customization applied to it
+     * (e.g. preserveMapKeys in MappingConfiguration) is shared with this template.
+     */
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient client, MappingMongoConverter converter) {
+        if (client == null) {
+            throw new IllegalArgumentException("MongoClient must not be null");
+        }
+        String databaseName = mongoProperties.getDatabase();
+        if (databaseName == null || databaseName.isEmpty()) {
+            throw new IllegalArgumentException("MongoDB database name must be provided");
+        }
+        MongoDatabaseFactory factory = new SimpleMongoClientDatabaseFactory(client, databaseName);
+        return new MongoTemplate(factory, converter);
+    }
+
+    @Bean(destroyMethod = "close")
+    public MongoClient mongoClient() {
+        String uri = mongoProperties.getUri();
+        if (uri == null || uri.isEmpty()) {
+            throw new IllegalArgumentException("MongoDB URI must be provided");
+        }
+        return MongoClients.create(uri);
     }
 
     /**
