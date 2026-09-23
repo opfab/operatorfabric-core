1c6abd29863bbd66bb8e75d2f2088fd704fe71f6
vlo-rte <valerie.longa@rte-france.com>
2026-09-23 16:14:50 +0200

    wip
    Signed-off-by: vlo-rte <valerie.longa@rte-france.com>

--- tests/cypress/cypress/integration/Resilience.spec.js
+++ tests/cypress/cypress/integration/Resilience.spec.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021-2025, RTE (http://www.rte-france.com)
+/* Copyright (c) 2021-2026, RTE (http://www.rte-france.com)
  * See AUTHORS.txt
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -78,7 +78,7 @@ describe('Resilience tests', function () {
     }
 
     function checkLoadingSpinnerIsVisible() {
-        cy.get('#opfab-connecting-spinner');
+        cy.get('#opfab-connecting-spinner').should('exist');
     }
 
     function startNginx() {
